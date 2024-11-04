0ac4493785d9b498da5dac4d2aec6b1352bc6155
Hellin22 <dno06117@naver.com>
2024-11-04 14:42:59 +0900

    기본 세팅 완료

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/LearnsMateApplication.java
@@ -0,0 +1,13 @@
+package intbyte4.learnsmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnsMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnsMateApplication.class, args);
+    }
+
+}
---
+++ LearnsMate/src/test/java/intbyte4/learnsmate/LearnsMateApplicationTests.java
@@ -0,0 +1,13 @@
+package intbyte4.learnsmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnsMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
