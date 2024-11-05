07f80b3de95fecceec928c5ee7b47b15cd729e4d
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-05 14:18:59 +0900

    ♻️ 코드 수정 : 캠페인 템플릿 서비스 Mapper 메소드 삭제

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaigntemplate/service/CampaignTemplateServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaigntemplate/service/CampaignTemplateServiceImpl.java
@@ -5,6 +5,7 @@ import intbyte4.learnsmate.admin.domain.entity.Admin;
 import intbyte4.learnsmate.admin.service.AdminService;
 import intbyte4.learnsmate.campaigntemplate.domain.CampaignTemplate;
 import intbyte4.learnsmate.campaigntemplate.domain.dto.CampaignTemplateDTO;
+import intbyte4.learnsmate.campaigntemplate.mapper.CampaignTemplateMapper;
 import intbyte4.learnsmate.campaigntemplate.repository.CampaignTemplateRepository;
 import intbyte4.learnsmate.common.exception.CommonException;
 import intbyte4.learnsmate.common.exception.StatusEnum;
@@ -13,14 +14,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-
-@RequiredArgsConstructor
 @Slf4j
 @Service("campaignTemplateService")
+@RequiredArgsConstructor
 public class CampaignTemplateServiceImpl implements CampaignTemplateService {
 
     private final CampaignTemplateRepository campaignTemplateRepository;
+    private final CampaignTemplateMapper campaignTemplateMapper;
     private final AdminService adminService;
 
     @Override
@@ -32,30 +32,18 @@ public class CampaignTemplateServiceImpl implements CampaignTemplateService {
         AdminDTO adminDTO = adminService.findByAdminCode(campaignTemplateDTO.getAdminCode());
         if (adminDTO == null) {
             log.warn("존재하지 않는 사용자 : {}", campaignTemplateDTO.getAdminCode());
-            throw new CommonException(StatusEnum.USER_NOT_FOUND);
+            throw new CommonException(StatusEnum.ADMIN_NOT_FOUND);
         }
         log.info(adminDTO.toString());
 
         Admin user = adminDTO.convertToEntity();
-        CampaignTemplate campaignTemplate = convertToTemplate(campaignTemplateDTO, user);
+        CampaignTemplate campaignTemplate = campaignTemplateMapper.toEntity(campaignTemplateDTO, user);
 
         log.info("데이터베이스에 템플릿 저장 중: {}", campaignTemplate);
-        CampaignTemplate saveCampaignTemplate = campaignTemplateRepository.save(campaignTemplate);
-        log.info("저장된 템플릿 객체: {}", saveCampaignTemplate);
+        CampaignTemplate savedCampaignTemplate = campaignTemplateRepository.save(campaignTemplate);
+        log.info("저장된 템플릿 객체: {}", savedCampaignTemplate);
 
-        return saveCampaignTemplate.convertToCampaignDTO();
+        return campaignTemplateMapper.fromEntityToDto(savedCampaignTemplate);
     }
 
-    private CampaignTemplate convertToTemplate(CampaignTemplateDTO campaignTemplateDTO, Admin admin) {
-        CampaignTemplate campaignTemplate = new CampaignTemplate();
-        campaignTemplate.setCampaignTemplateCode(campaignTemplateDTO.getCampaignTemplateCode());
-        campaignTemplate.setCampaignTemplateTitle(campaignTemplateDTO.getCampaignTemplateTitle());
-        campaignTemplate.setCampaignTemplateContents(campaignTemplateDTO.getCampaignTemplateContents());
-        campaignTemplate.setCampaignTemplateFlag(campaignTemplateDTO.getCampaignTemplateFlag());
-        campaignTemplate.setCreatedAt(LocalDateTime.now());
-        campaignTemplate.setUpdatedAt(LocalDateTime.now());
-        campaignTemplate.setAdmin(admin);
-
-        return campaignTemplate;
-    }
 }
