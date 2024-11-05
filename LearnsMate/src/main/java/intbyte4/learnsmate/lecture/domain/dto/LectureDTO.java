90b32f7ad4d6eadb06aafa05eb3e2b9f3e51e55c
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-05 11:22:28 +0900

    ✨ 기능 추가 : Lecture 형태 변환 메소드 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/dto/LectureDTO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/dto/LectureDTO.java
@@ -1,26 +1,67 @@
 package intbyte4.learnsmate.lecture.domain.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import intbyte4.learnsmate.lecture.domain.entity.Lecture;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
+@Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class LectureDTO {
+    @JsonProperty("lecture_code")
     private Long lectureCode;
+
+    @JsonProperty("lecture_title")
     private String lectureTitle;
+
+    @JsonProperty("lecture_category")
     private Integer lectureCategory;
+
+    @JsonProperty("lecture_confirm_status")
     private Boolean lectureConfirmStatus;
+
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
+
+    @JsonProperty("lecture_image")
     private String lectureImage;
+
+    @JsonProperty("lecture_price")
     private Integer lecturePrice;
+
+    @JsonProperty("tutor_code")
     private Long tutorCode;
+
+    @JsonProperty("lecture_status")
     private Boolean lectureStatus;
+
+    @JsonProperty("lecture_click_count")
     private Integer lectureClickCount;
+
+    @JsonProperty("lecture_level")
     private Integer lectureLevel;
+
+    public Lecture toLecture() {
+        return Lecture.builder()
+                .lectureCode(this.lectureCode)
+                .lectureTitle(this.lectureTitle)
+                .lectureCategory(this.lectureCategory)
+                .lectureConfirmStatus(this.lectureConfirmStatus)
+                .createdAt(this.createdAt)
+                .updatedAt(this.updatedAt)
+                .lectureImage(this.lectureImage)
+                .lecturePrice(this.lecturePrice)
+                .lectureStatus(this.lectureStatus)
+                .lectureClickCount(this.lectureClickCount)
+                .lectureLevel(this.lectureLevel)
+                .build();
+    }
 }
--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/entity/Lecture.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/entity/Lecture.java
@@ -1,5 +1,6 @@
 package intbyte4.learnsmate.lecture.domain.entity;
 
+import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
 import intbyte4.learnsmate.member.domain.entity.Member;
 import jakarta.persistence.*;
 import lombok.*;
@@ -54,4 +55,21 @@ public class Lecture {
 
     @Column(name = "lecture_level", nullable = false)
     private Integer lectureLevel;
+
+    public LectureDTO convertToDTO() {
+        return LectureDTO.builder()
+                .lectureCode(this.lectureCode)
+                .lectureTitle(this.lectureTitle)
+                .lectureCategory(this.lectureCategory)
+                .lectureConfirmStatus(this.lectureConfirmStatus)
+                .createdAt(this.createdAt)
+                .updatedAt(this.updatedAt)
+                .lectureImage(this.lectureImage)
+                .lecturePrice(this.lecturePrice)
+                .lectureStatus(this.lectureStatus)
+                .lectureClickCount(this.lectureClickCount)
+                .lectureLevel(this.lectureLevel)
+                .build();
+
+    }
 }
