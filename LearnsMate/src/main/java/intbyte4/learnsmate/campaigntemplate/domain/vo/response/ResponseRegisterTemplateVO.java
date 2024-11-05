f46eea156174d56492c9c4d97d646e83a787f35b
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-05 15:20:31 +0900

    ✨ 기능 추가 : 캠페인 템플릿 등록 응답 VO - JsonProperty 어노테이션 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaigntemplate/domain/vo/response/ResponseRegisterTemplateVO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaigntemplate/domain/vo/response/ResponseRegisterTemplateVO.java
@@ -1,5 +1,6 @@
 package intbyte4.learnsmate.campaigntemplate.domain.vo.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -11,11 +12,24 @@ import java.time.LocalDateTime;
 @Builder
 public class ResponseRegisterTemplateVO {
 
+    @JsonProperty("campaign_template_code")
     private final Long campaignTemplateCode;
+
+    @JsonProperty("campaign_template_title")
     private final String campaignTemplateTitle;
+
+    @JsonProperty("campaign_template_contents")
     private final String campaignTemplateContents;
+
+    @JsonProperty("campaign_template_flag")
     private final Boolean campaignTemplateFlag;
+
+    @JsonProperty("created_at")
     private final LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
     private final LocalDateTime updatedAt;
+
+    @JsonProperty("admin_code")
     private final Long adminCode;
 }
