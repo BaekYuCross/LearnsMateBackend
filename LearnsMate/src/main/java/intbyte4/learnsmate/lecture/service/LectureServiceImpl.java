7fb39410b9d55bd55fd8a7bcb2fd112fb840a8de
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-05 11:22:52 +0900

    ✨ 기능 추가 : Lecture List 전체 조회 Service 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/service/LectureServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/service/LectureServiceImpl.java
@@ -1,10 +1,36 @@
 package intbyte4.learnsmate.lecture.service;
 
 
+import intbyte4.learnsmate.common.exception.CommonException;
+import intbyte4.learnsmate.common.exception.StatusEnum;
+import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
+import intbyte4.learnsmate.lecture.domain.entity.Lecture;
+import intbyte4.learnsmate.lecture.repository.LectureRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LectureServiceImpl implements LectureService {
+    
+    private final LectureRepository lectureRepository;
+
+    // 전체 강의 조회
+    public List<LectureDTO> getAllLecture() {
+        List<Lecture> lectureList = lectureRepository.findAll();
+        if (lectureList.isEmpty()) throw new CommonException(StatusEnum.LECTURE_NOT_FOUND);
+        return lectureList.stream()
+                .map(Lecture::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    // 강의 단건 조회
+    // 카테고리별 강의 조회
+
+
+
 }
