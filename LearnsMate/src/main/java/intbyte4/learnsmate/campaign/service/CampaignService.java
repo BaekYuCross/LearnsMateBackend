1600db9d05dd6d9b6827ed1030aac51c7bdb8116
YuJeeun <dbwpdms1122@naver.com>
2024-11-05 17:18:38 +0900

    ✨ 기능 추가: CampaignService -> 캠페인 취소 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/service/CampaignService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/service/CampaignService.java
@@ -5,4 +5,5 @@ import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
 public interface CampaignService {
     CampaignDTO registerCampaign(CampaignDTO request);
     CampaignDTO editCampaign(CampaignDTO request, Long campaignCode);
+    void removeCampaign(CampaignDTO request);
 }
