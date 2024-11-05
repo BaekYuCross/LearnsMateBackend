87191e576058e368902df123324eb4f19320618c
YuJeeun <dbwpdms1122@naver.com>
2024-11-05 17:17:49 +0900

    ♻️ 코드 수정: VO에 JsonProperty 어노테이션 작성 및 DTO에 있던 어노테이션 제거

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/vo/request/RequestRegisterCampaignVO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/vo/request/RequestRegisterCampaignVO.java
@@ -1,5 +1,6 @@
 package intbyte4.learnsmate.campaign.domain.vo.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,28 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class RequestRegisterCampaignVO {
+    @JsonProperty("campaign_code")
     private Long campaignCode;
+
+    @JsonProperty("campaign_title")
     private String campaignTitle;
+
+    @JsonProperty("campaign_contents")
     private String campaignContents;
+
+    @JsonProperty("campaign_type")
     private String campaignType;
+
+    @JsonProperty("campaign_send_date")
     private LocalDateTime campaignSendDate;
+
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
+
+    @JsonProperty("admin_code")
     private Long adminCode;
 
 }
--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/vo/response/ResponseRegisterCampaignVO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/vo/response/ResponseRegisterCampaignVO.java
@@ -1,5 +1,6 @@
 package intbyte4.learnsmate.campaign.domain.vo.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,13 +11,28 @@ import java.time.LocalDateTime;
 @Setter
 @Builder
 public class ResponseRegisterCampaignVO {
+    @JsonProperty("campaign_code")
     private Long campaignCode;
+
+    @JsonProperty("campaign_title")
     private String campaignTitle;
+
+    @JsonProperty("campaign_contents")
     private String campaignContents;
+
+    @JsonProperty("campaign_type")
     private String campaignType;
+
+    @JsonProperty("campaign_send_date")
     private LocalDateTime campaignSendDate;
+
+    @JsonProperty("created_at")
     private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
+
+    @JsonProperty("admin_code")
     private Long adminCode;
 
 }
