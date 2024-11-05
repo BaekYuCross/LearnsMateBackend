591eb54456f9a1fd79e12db8b3ffc78800a90fac
YuJeeun <dbwpdms1122@naver.com>
2024-11-05 11:16:30 +0900

    ✨ 기능 추가: 캠페인 등록 기능을 위한 registCampaign 메서드 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/service/CampaignService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/service/CampaignService.java
@@ -1,5 +1,8 @@
 package intbyte4.learnsmate.campaign.service;
 
+import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
+
 public interface CampaignService {
+    CampaignDTO registCampaign(CampaignDTO request);
 
 }
--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/service/CampaignServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/service/CampaignServiceImpl.java
@@ -1,10 +1,53 @@
 package intbyte4.learnsmate.campaign.service;
 
+import intbyte4.learnsmate.admin.domain.dto.AdminDTO;
+import intbyte4.learnsmate.admin.domain.entity.Admin;
+import intbyte4.learnsmate.admin.service.AdminService;
+import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
+import intbyte4.learnsmate.campaign.domain.entity.Campaign;
+import intbyte4.learnsmate.campaign.domain.entity.CampaignTypeEnum;
+import intbyte4.learnsmate.campaign.repository.CampaignRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
-public class CampaignServiceImpl implements CampaignService{
+public class CampaignServiceImpl implements CampaignService {
+    private final CampaignRepository campaignRepository;
+    private final AdminService adminService;
+
+    @Override
+    public CampaignDTO registCampaign(CampaignDTO request) {
+
+        LocalDateTime sendTime;
+
+        AdminDTO adminDTO = adminService.findByAdminCode(request.getAdminCode());
+        Admin admin = adminDTO.convertToEntity();
+
+        if (Objects.equals(request.getCampaignType(), CampaignTypeEnum.INSTANT.getType())){
+            sendTime = LocalDateTime.now();
+        } else {
+            sendTime = request.getCampaignSendDate();
+        };
+
+        Campaign campaign = Campaign.builder()
+                .campaignCode(null)
+                .campaignTitle(request.getCampaignTitle())
+                .campaignContents(request.getCampaignContents())
+                .campaignType(CampaignTypeEnum.valueOf(request.getCampaignType()))
+                .campaignSendDate(sendTime)
+                .createdAt(request.getCreatedAt())
+                .updatedAt(LocalDateTime.now())
+                .admin(admin)
+                .build();
+
+        campaignRepository.save(campaign);
+
+        return campaign.toDTO();
+    }
 
 }
