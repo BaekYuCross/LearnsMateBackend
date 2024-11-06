4c72b71ffa76d2ed6f3db18776bae9ae1661fbe8
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-06 11:37:46 +0900

    ✨ 기능 추가 : Lecture 전체 조회 Controller 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/controller/LectureController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/controller/LectureController.java
@@ -1,7 +1,33 @@
 package intbyte4.learnsmate.lecture.controller;
 
+import intbyte4.learnsmate.lecture.domain.vo.response.ResponseFindLectureVO;
+import intbyte4.learnsmate.lecture.mapper.LectureMapper;
+import intbyte4.learnsmate.lecture.service.LectureService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/lecture")
+@Slf4j
+@RequiredArgsConstructor
 public class LectureController {
+
+    private final LectureService lectureService;
+    private final LectureMapper lectureMapper;
+
+    @Operation(summary = "강의 정보 전체 조회")
+    @GetMapping
+    public ResponseEntity<List<ResponseFindLectureVO>> getAllLectures() {
+        List<ResponseFindLectureVO> lectures = lectureService.getAllLecture();
+        return new ResponseEntity<>(lectures, HttpStatus.OK);
+    }
+
 }
