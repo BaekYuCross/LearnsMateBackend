9449d1d787cb95eede65ded5626354edf70233fd
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-06 11:38:50 +0900

    ✨ 기능 추가 : Lecture 수정 반응VO 기능 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/vo/response/ResponseEditLectureInfoVO.java
@@ -0,0 +1,26 @@
+package intbyte4.learnsmate.lecture.domain.vo.response;
+
+import intbyte4.learnsmate.lecture.enums.LectureCategoryEnum;
+import intbyte4.learnsmate.lecture.enums.LectureLevelEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ResponseEditLectureInfoVO {
+    private String lectureTitle;
+    private LectureCategoryEnum lectureCategoryEnum;
+    private Boolean lectureConfirmStatus;
+    private LocalDateTime updatedAt;
+    private String lectureImage;
+    private Integer lecturePrice;
+    private Boolean lectureStatus;
+    private Integer lectureClickCount;
+    private LectureLevelEnum lectureLevel;
+}
