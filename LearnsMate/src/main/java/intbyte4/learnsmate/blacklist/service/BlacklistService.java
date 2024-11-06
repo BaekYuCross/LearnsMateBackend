e892a487a1a499723d697c31e6c789e324cad9b0
Hellin22 <dno06117@naver.com>
2024-11-06 10:41:08 +0900

    ✨ 기능 추가 / 수정: Blacklist 조회 Service 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/blacklist/service/BlacklistService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/blacklist/service/BlacklistService.java
@@ -1,16 +1,44 @@
 package intbyte4.learnsmate.blacklist.service;
 
+import intbyte4.learnsmate.blacklist.domain.dto.BlacklistDTO;
+import intbyte4.learnsmate.blacklist.domain.entity.Blacklist;
+import intbyte4.learnsmate.blacklist.mapper.BlacklistMapper;
 import intbyte4.learnsmate.blacklist.repository.BlacklistRepository;
+import intbyte4.learnsmate.member.domain.MemberType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BlacklistService {
 
     private final BlacklistRepository blacklistRepository;
+    private final BlacklistMapper blacklistMapper;
 
     @Autowired
-    public BlacklistService(BlacklistRepository blacklistRepository) {
+    public BlacklistService(BlacklistRepository blacklistRepository, BlacklistMapper blacklistMapper) {
         this.blacklistRepository = blacklistRepository;
+        this.blacklistMapper = blacklistMapper;
+    }
+
+    // 1. flag는 볼필요 없음. -> 학생, 강사만 구분해야함.
+    public List<BlacklistDTO> findAllBlacklistByMemberType(MemberType memberType) {
+
+        List<Blacklist> blacklistList = blacklistRepository.findAllBlacklistByMemberType(memberType);
+
+        List<BlacklistDTO> blacklistDTOList = new ArrayList<>();
+
+//                .memberCode(blacklist.getMemberCode())
+//                .reportCode(blacklist.getReportCode())
+//                .adminCode(blacklist.getAdminCode())
+
+        for (Blacklist blacklist : blacklistList) {
+            // Blacklsit -> BlacklsitDTO
+            blacklistDTOList.add(blacklistMapper.fromBlacklistToBlacklistDTO(blacklist));
+        }
+
+        return blacklistDTOList;
     }
 }
