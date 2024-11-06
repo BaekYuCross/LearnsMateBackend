a9045753247f2c593e757c9d87c5da0ff4b19627
Hellin22 <dno06117@naver.com>
2024-11-06 10:40:27 +0900

    ✨ 기능 추가 / 수정: Blacklist 도메인 수정

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/blacklist/domain/vo/response/ResponseFindReportVO.java
@@ -0,0 +1,20 @@
+package intbyte4.learnsmate.blacklist.domain.vo.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ResponseFindReportVO {
+
+    private Long blackCode;
+    private String blackReason;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long memberCode;
+    private Long reportCode;
+    private Long adminCode;}
