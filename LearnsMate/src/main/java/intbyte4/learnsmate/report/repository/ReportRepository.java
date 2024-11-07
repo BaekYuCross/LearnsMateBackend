1ac218842c42aa11b9641703497b590071abcbf4
Hellin22 <dno06117@naver.com>
2024-11-07 21:27:40 +0900

    ✨ 기능 추가 / 수정: ReportService -> Report table에서 신고횟수가 5회 이상 누적된 사람 list 구하기
    ReportedMemberDTO(Member, ReportCount)

--- LearnsMate/src/main/java/intbyte4/learnsmate/report/repository/ReportRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/report/repository/ReportRepository.java
@@ -1,9 +1,18 @@
 package intbyte4.learnsmate.report.repository;
 
+import intbyte4.learnsmate.member.domain.entity.Member;
+import intbyte4.learnsmate.report.domain.dto.ReportedMemberDTO;
 import intbyte4.learnsmate.report.domain.entity.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     long countByReportedMember_MemberCode(Long memberCode);
+
+    @Query("SELECT new intbyte4.learnsmate.report.domain.dto.ReportedMemberDTO(r.reportedMember, COUNT(r)) " +
+            "FROM report r GROUP BY r.reportedMember HAVING COUNT(r) >= 5")
+    List<ReportedMemberDTO> findMembersWithReportsCountGreaterThanEqualFive();
 }
