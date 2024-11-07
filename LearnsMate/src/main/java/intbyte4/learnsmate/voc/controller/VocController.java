4a525bd8bed2de7efb4b2977667248f5b921a1f5
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-07 21:18:18 +0900

    ✨ 기능 추가 : VOC - 단 건 조회 API 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/voc/controller/VocController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/voc/controller/VocController.java
@@ -1,5 +1,6 @@
 package intbyte4.learnsmate.voc.controller;
 
+import intbyte4.learnsmate.common.exception.CommonException;
 import intbyte4.learnsmate.voc.domain.dto.VocDTO;
 import intbyte4.learnsmate.voc.domain.vo.response.ResponseFindVocVO;
 import intbyte4.learnsmate.voc.mapper.VocMapper;
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +39,27 @@ public class VocController {
         }
 
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Operation(summary = "직원 - VOC 단 건 조회")
+    @GetMapping("/{vocCode}")
+    public ResponseEntity<?> getTemplate(@PathVariable("vocCode") Long vocCode) {
+        log.info("조회 요청된 VOC 코드 : {}", vocCode);
+        try {
+            VocDTO vocDTO = new VocDTO();
+            vocDTO.setVocCode(vocCode);
+
+            VocDTO findVocDTO = vocService.findByVocCode(vocDTO);
+            ResponseFindVocVO response = vocMapper.fromDtoToFindResponseVO(findVocDTO);
+
+            log.info("캠페인 템플릿 조회 성공: {}", response);
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        } catch (CommonException e) {
+            log.error("캠페인 템플릿 조회 오류: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            log.error("예상치 못한 오류", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예상치 못한 오류가 발생했습니다");
+        }
     }
 }
