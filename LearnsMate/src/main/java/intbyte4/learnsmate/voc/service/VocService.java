fcee7e00285f2bc3e1bdd1ddadb307b4eaa01a4e
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-07 20:58:00 +0900

    ✨ 기능 추가 : VOC - 전체 조회 비즈니스 로직 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/voc/service/VocService.java
@@ -0,0 +1,9 @@
+package intbyte4.learnsmate.voc.service;
+
+import intbyte4.learnsmate.voc.domain.dto.VocDTO;
+
+import java.util.List;
+
+public interface VocService {
+    List<VocDTO> findAllByVoc();
+}
