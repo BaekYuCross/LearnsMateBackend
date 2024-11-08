b0952c8af605d327020178b4cf6ada301958c149
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-08 11:12:14 +0900

    ✨ 기능 추가 : VOC 답변 서비스 등록 기능 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/voc_answer/service/VocAnswerService.java
@@ -0,0 +1,7 @@
+package intbyte4.learnsmate.voc_answer.service;
+
+import intbyte4.learnsmate.voc_answer.domain.dto.VocAnswerDTO;
+
+public interface VocAnswerService {
+    VocAnswerDTO registerVocAnswer(VocAnswerDTO vocAnswerDTO);
+}
