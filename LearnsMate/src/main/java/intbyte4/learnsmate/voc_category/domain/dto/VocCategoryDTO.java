0de35cd3ec686aabc820e22943a37bbf4ccaba03
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-08 10:30:20 +0900

    ✨ 기능 추가 : VOC - 상위 카테고리 DTO 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/voc_category/domain/dto/VocCategoryDTO.java
@@ -0,0 +1,14 @@
+package intbyte4.learnsmate.voc_category.domain.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class VocCategoryDTO {
+    private Integer vocCategoryCode;
+    private String vocCategoryName;
+}
