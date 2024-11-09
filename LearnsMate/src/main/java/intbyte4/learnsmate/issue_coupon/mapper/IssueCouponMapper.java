ea693cf04fc995f133a398f8eee00c3d1027904e
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-09 18:16:43 +0900

    ✨ 기능 추가 : 발급 쿠폰 Mapper에 코드추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/issue_coupon/mapper/IssueCouponMapper.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/issue_coupon/mapper/IssueCouponMapper.java
@@ -2,6 +2,7 @@ package intbyte4.learnsmate.issue_coupon.mapper;
 
 import intbyte4.learnsmate.issue_coupon.domain.IssueCoupon;
 import intbyte4.learnsmate.issue_coupon.domain.dto.IssueCouponDTO;
+import intbyte4.learnsmate.issue_coupon.domain.vo.response.IssueCouponFindResponseVO;
 import intbyte4.learnsmate.issue_coupon.domain.vo.response.IssueCouponRegisterResponseVO;
 import org.springframework.stereotype.Component;
 
@@ -18,14 +19,17 @@ public class IssueCouponMapper {
                 .build();
     }
 
-    public IssueCouponRegisterResponseVO fromDtoToResponseVO(IssueCouponDTO issueCoupon) {
+    public IssueCouponRegisterResponseVO fromDtoToRegisterResponseVO(IssueCouponDTO issueCoupon) {
         return IssueCouponRegisterResponseVO.builder()
                 .couponIssuanceCode(issueCoupon.getCouponIssuanceCode())
                 .couponIssueDate(issueCoupon.getCouponIssueDate())
-                .couponUseStatus(issueCoupon.getCouponUseStatus())
-                .couponUseDate(issueCoupon.getCouponUseDate())
-                .studentCode(issueCoupon.getStudentCode())
-                .couponCode(issueCoupon.getCouponCode())
+                .build();
+    }
+
+    public IssueCouponFindResponseVO fromDtoToFindResponseVO(IssueCouponDTO issueCouponDTO) {
+        return IssueCouponFindResponseVO.builder()
+                .couponIssuanceCode(issueCouponDTO.getCouponIssuanceCode())
+                .couponIssueDate(issueCouponDTO.getCouponIssueDate())
                 .build();
     }
 }
