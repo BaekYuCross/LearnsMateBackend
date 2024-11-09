6c86b6ccafa420b5c9f7c8ac6ac4cde8f07ed198
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-09 18:59:06 +0900

    ✨ 기능 추가 : findByStudentCode Repository 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecturebystudent/repository/LectureByStudentRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecturebystudent/repository/LectureByStudentRepository.java
@@ -4,6 +4,9 @@ import intbyte4.learnsmate.lecturebystudent.domain.entity.LectureByStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LectureByStudentRepository extends JpaRepository<LectureByStudent, Long> {
+    List<LectureByStudent> findByStudentCode(Long studentCode);
 }
