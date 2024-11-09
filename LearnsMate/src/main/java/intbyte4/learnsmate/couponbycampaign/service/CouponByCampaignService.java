637fd6dee227ff15ab664fcbfedbbd4b6e33ed65
YuJeeun <dbwpdms1122@naver.com>
2024-11-09 19:56:58 +0900

    ✨ 기능 추가: CouponByCampaignService -> findByCampaignCode, removeCouponByCampaign 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/couponbycampaign/service/CouponByCampaignService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/couponbycampaign/service/CouponByCampaignService.java
@@ -1,8 +1,14 @@
 package intbyte4.learnsmate.couponbycampaign.service;
 
 import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
+import intbyte4.learnsmate.campaign.domain.entity.Campaign;
 import intbyte4.learnsmate.coupon.domain.dto.CouponDTO;
+import intbyte4.learnsmate.couponbycampaign.domain.dto.CouponByCampaignDTO;
+
+import java.util.List;
 
 public interface CouponByCampaignService {
     void registerCouponByCampaign(CouponDTO coupon, CampaignDTO campaign);
+    List<CouponByCampaignDTO> findByCampaignCode(Campaign updatedCampaign);
+    void removeCouponByCampaign(Long couponByCampaignCode);
 }
