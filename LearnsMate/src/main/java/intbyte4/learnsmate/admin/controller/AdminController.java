7678ea0907b135e30e13403d4d2019d83eefbc77
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-09 22:23:19 +0900

    ✨ 기능 추가 : AdminController 직원 단건 조회 Controller 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/admin/controller/AdminController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/admin/controller/AdminController.java
@@ -1,7 +1,31 @@
 package intbyte4.learnsmate.admin.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import intbyte4.learnsmate.admin.domain.dto.AdminDTO;
+import intbyte4.learnsmate.admin.domain.vo.request.RequestEditAdminVO;
+import intbyte4.learnsmate.admin.domain.vo.response.ResponseEditAdminVO;
+import intbyte4.learnsmate.admin.domain.vo.response.ResponseFindAdminVO;
+import intbyte4.learnsmate.admin.mapper.AdminMapper;
+import intbyte4.learnsmate.admin.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
+    private final AdminMapper adminMapper;
+
+    @Operation(summary = "직원 단건 조회")
+    @GetMapping("/{adminCode}")
+    public ResponseEntity<ResponseFindAdminVO> getAdmin(@PathVariable Long adminCode) {
+        AdminDTO updatedAdmin = adminService.findByAdminCode(adminCode);
+        return ResponseEntity.status(HttpStatus.OK).body(adminMapper.fromDtoToFindResponseVO(updatedAdmin));
+    }
+
+
 }
