13a1603e72e386aea33060919e26e0868fef473d
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-09 16:45:10 +0900

    ✨ 기능 추가 : findAllByTutorCode기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/repository/LectureRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/repository/LectureRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture,Long> , JpaSpecificationExecutor<Lecture> {
+    List<Lecture> findAllByTutorCode(Long tutorCode);
 }
