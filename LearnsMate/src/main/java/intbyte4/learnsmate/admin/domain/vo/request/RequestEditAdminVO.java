43814949d51566f8a1469cf9d1763479b29befe8
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-09 22:50:31 +0900

    ✨ 기능 추가 :JsonNaming 스네이크 케이스로 변환 기능 수정

--- LearnsMate/src/main/java/intbyte4/learnsmate/admin/domain/vo/request/RequestEditAdminVO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/admin/domain/vo/request/RequestEditAdminVO.java
@@ -1,6 +1,8 @@
 package intbyte4.learnsmate.admin.domain.vo.request;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -10,26 +12,14 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RequestEditAdminVO {
 
-    @JsonProperty("admin_email")
     private String adminEmail;
-
-    @JsonProperty("admin_password")
     private String adminPassword;
-
-    @JsonProperty("admin_name")
     private String adminName;
-
-    @JsonProperty("admin_phone")
     private String adminPhone;
-
-    @JsonProperty("admin_address")
     private String adminAddress;
-
-    @JsonProperty("admin_birthday")
     private LocalDateTime adminBirthday;
-
-    @JsonProperty("updated_at")
     private LocalDateTime updatedAt;
 }
