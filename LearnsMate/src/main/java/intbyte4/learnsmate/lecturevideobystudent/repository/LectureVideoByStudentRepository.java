8fa2013bafbf36b72b22b812bee119198c96024a
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-09 18:58:12 +0900

    ✨ 기능 추가 : 강의에 속한 동영상의 총 개수 카운트 Repository 쿼리 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecturevideobystudent/repository/LectureVideoByStudentRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecturevideobystudent/repository/LectureVideoByStudentRepository.java
@@ -2,8 +2,16 @@ package intbyte4.learnsmate.lecturevideobystudent.repository;
 
 import intbyte4.learnsmate.lecturevideobystudent.domain.entity.LectureVideoByStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LectureVideoByStudentRepository extends JpaRepository<LectureVideoByStudent, Long> {
+
+    // 강의에 속한 동영상의 총 개수 카운트
+    @Query("SELECT COUNT(lvs) FROM LectureVideoByStudent lvs WHERE lvs.videoCode.lecture.lectureCode = :lectureCode")
+    long countByLectureCode(Long lectureCode);
+
+
 }
+
