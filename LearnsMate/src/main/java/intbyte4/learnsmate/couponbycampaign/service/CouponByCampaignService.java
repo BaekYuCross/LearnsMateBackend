6eafea0f8b8633cb7da5be752aa8ffcf51f00518
YuJeeun <dbwpdms1122@naver.com>
2024-11-09 17:19:26 +0900

    ✨ 기능 추가: CouponByCampaignService -> registerCouponByCampaign 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/couponbycampaign/service/CouponByCampaignService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/couponbycampaign/service/CouponByCampaignService.java
@@ -1,7 +1,8 @@
 package intbyte4.learnsmate.couponbycampaign.service;
 
+import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
 import intbyte4.learnsmate.coupon.domain.dto.CouponDTO;
 
 public interface CouponByCampaignService {
-    void registerCouponByCampaign(CouponDTO foundCoupon);
+    void registerCouponByCampaign(CouponDTO coupon, CampaignDTO campaign);
 }
--- LearnsMate/src/main/java/intbyte4/learnsmate/couponbycampaign/service/CouponByCampaignServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/couponbycampaign/service/CouponByCampaignServiceImpl.java
@@ -1,7 +1,35 @@
 package intbyte4.learnsmate.couponbycampaign.service;
 
+import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
+import intbyte4.learnsmate.campaign.domain.entity.Campaign;
+import intbyte4.learnsmate.coupon.domain.dto.CouponDTO;
+import intbyte4.learnsmate.coupon.domain.entity.CouponEntity;
+import intbyte4.learnsmate.couponbycampaign.domain.entity.CouponByCampaign;
+import intbyte4.learnsmate.couponbycampaign.repository.CouponByCampaignRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CouponByCampaignServiceImpl implements CouponByCampaignService{
+    private final CouponByCampaignRepository couponByCampaignRepository;
+
+    @Override
+    public void registerCouponByCampaign(CouponDTO coupon, CampaignDTO campaign) {
+        CouponEntity getCouponCode = CouponEntity.builder()
+                .couponCode(coupon.getCouponCode())
+                .build();
+
+        Campaign getCampaignCode = Campaign.builder()
+                .campaignCode(campaign.getCampaignCode())
+                .build();
+
+        CouponByCampaign couponByCampaign = CouponByCampaign.builder()
+                .couponByCampaignCode(null)
+                .coupon(getCouponCode)
+                .campaign(getCampaignCode)
+                .build();
+
+        couponByCampaignRepository.save(couponByCampaign);
+    }
 }
