c358a109d16d30f1bf319dec4892b29de3555c24
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-09 19:57:50 +0900

    ✨ 기능 추가/수정 : 학생이 발급된 쿠폰을 사용했을 때의 API 추가 및 Http 요청 관련 수정

--- LearnsMate/src/main/java/intbyte4/learnsmate/issue_coupon/controller/IssueCouponController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/issue_coupon/controller/IssueCouponController.java
@@ -4,6 +4,7 @@ import intbyte4.learnsmate.issue_coupon.domain.dto.IssueCouponDTO;
 import intbyte4.learnsmate.issue_coupon.domain.vo.request.IssueCouponRegisterRequestVO;
 import intbyte4.learnsmate.issue_coupon.domain.vo.response.IssueCouponFindResponseVO;
 import intbyte4.learnsmate.issue_coupon.domain.vo.response.IssueCouponRegisterResponseVO;
+import intbyte4.learnsmate.issue_coupon.domain.vo.response.IssueCouponUseResponseVO;
 import intbyte4.learnsmate.issue_coupon.mapper.IssueCouponMapper;
 import intbyte4.learnsmate.issue_coupon.service.IssueCouponService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -37,7 +38,7 @@ public class IssueCouponController {
     }
 
     @Operation(summary = "학생이 자신의 발급된 쿠폰 리스트 조회")
-    @PostMapping("/list")
+    @GetMapping("/list")
     public ResponseEntity<List<IssueCouponFindResponseVO>> findIssuedCoupons(@RequestParam("student_code") Long studentCode) {
         List<IssueCouponDTO> issuedCoupons = issueCouponService.findIssuedCouponsByStudent(studentCode);
         List<IssueCouponFindResponseVO> responseList = issuedCoupons.stream()
@@ -45,6 +46,17 @@ public class IssueCouponController {
                 .collect(Collectors.toList());
 
         return new ResponseEntity<>(responseList, HttpStatus.OK);
+    }
+
+    @Operation(summary = "학생이 발급된 쿠폰 사용")
+    @PatchMapping("/use")
+    public ResponseEntity<IssueCouponUseResponseVO> useIssuedCoupon(
+            @RequestParam("student_code") Long studentCode,
+            @RequestParam("coupon_issuance_code") String couponIssuanceCode) {
+        IssueCouponDTO usedCoupon = issueCouponService.useIssuedCoupon(studentCode, couponIssuanceCode);
+        IssueCouponUseResponseVO response = issueCouponMapper.fromDtoToUseResponseVO(usedCoupon);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
