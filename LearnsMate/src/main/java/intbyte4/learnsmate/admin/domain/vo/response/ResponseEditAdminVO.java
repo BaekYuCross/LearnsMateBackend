8143efec9cc9609ba0c836af91bdacc40fbb07af
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-09 22:26:20 +0900

    ✨ 기능 추가 : ResponseEditAdminVO 기능 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/admin/domain/vo/response/ResponseEditAdminVO.java
@@ -0,0 +1,59 @@
+package intbyte4.learnsmate.admin.domain.vo.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ResponseEditAdminVO {
+
+    @JsonProperty("admin_code")
+    private Long adminCode;
+
+    @JsonProperty("admin_email")
+    private String adminEmail;
+
+    @JsonProperty("admin_password")
+    private String adminPassword;
+
+    @JsonProperty("admin_department")
+    private String adminDepartment;
+
+    @JsonProperty("admin_position")
+    private String adminPosition;
+
+    @JsonProperty("admin_name")
+    private String adminName;
+
+    @JsonProperty("admin_phone")
+    private String adminPhone;
+
+    @JsonProperty("admin_address")
+    private String adminAddress;
+
+    @JsonProperty("admin_birthday")
+    private LocalDateTime adminBirthday;
+
+    @JsonProperty("admin_job_type")
+    private String adminJobType;
+
+    @JsonProperty("admin_level")
+    private String adminLevel;
+
+    @JsonProperty("admin_status")
+    private Boolean adminStatus;
+
+    @JsonProperty("admin_last_login_date")
+    private LocalDateTime adminLastLoginDate;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+}
