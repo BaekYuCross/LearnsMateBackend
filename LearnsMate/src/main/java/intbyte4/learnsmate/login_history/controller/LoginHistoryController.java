ddde05bd4f1daab773e866dbb90e4ee003fd15a9
Hellin22 <dno06117@naver.com>
2024-11-10 15:28:48 +0900

    ✨ 기능 추가 / 수정: LoginHistoryController 전체조회, 코드로 조히하는 메서드 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/login_history/controller/LoginHistoryController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/login_history/controller/LoginHistoryController.java
@@ -1,14 +1,60 @@
 package intbyte4.learnsmate.login_history.controller;
 
+import intbyte4.learnsmate.login_history.domain.dto.LoginHistoryDTO;
+import intbyte4.learnsmate.login_history.domain.vo.response.ResponseFindLoginHistoryVO;
 import intbyte4.learnsmate.login_history.mapper.LoginHistoryMapper;
 import intbyte4.learnsmate.login_history.service.LoginHistoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/loginhistory")
 public class LoginHistoryController {
 
     private final LoginHistoryService loginHistoryService;
     private final LoginHistoryMapper loginHistoryMapper;
+
+    // 1. 모든 Login History 기록 조회
+    @GetMapping
+    public ResponseEntity<List<ResponseFindLoginHistoryVO>> findAllLoginHistory(){
+
+        List<LoginHistoryDTO> loginHistoryDTOList = loginHistoryService.findAllLoginHistory();
+
+        List<ResponseFindLoginHistoryVO> responseList =
+                loginHistoryMapper.fromLoginHistoryDTOToResponseFindLoginHistoryVO(loginHistoryDTOList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseList);
+    }
+
+    // 2. 특정 Login History 기록 조회
+    @GetMapping("/{loginhistorycode}")
+    public ResponseEntity<?> findLoginHistoryByLoginHistoryCode(@PathVariable("loginhistorycode") Long loginHistoryCode) {
+        LoginHistoryDTO loginHistoryDTO = loginHistoryService.findById(loginHistoryCode);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(loginHistoryMapper.fromLoginHistoryDTOToResponseFindLoginHistoryVO(loginHistoryDTO));
+    }
+
+    // 3. 특정 멤버의 모든 로그인 기록 확인
+    @GetMapping("/member/{membercode}")
+    public ResponseEntity<?> findAllLoginHistoryByMemberCode(@PathVariable Long membercode) {
+        return null;
+    }
+
+    // 4. 특정 멤버의 모든 로그아웃 기록 확인
+    @GetMapping("/member/1/{membercode}")
+    public ResponseEntity<?> findAllLogoutHistoryByMemberCode(@PathVariable Long membercode){
+        return null;
+    }
+
+
+
 }
