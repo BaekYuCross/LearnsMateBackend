3c63385119df917a4f6c974d36c57a9ef679d1f6
Hellin22 <dno06117@naver.com>
2024-11-10 16:25:18 +0900

    ✨ 기능 추가 / 수정: 로그인 할 시 로그인 내역에 추가하는 메서드 추가(로그아웃은 아직 없음)

--- LearnsMate/src/main/java/intbyte4/learnsmate/login_history/controller/LoginHistoryController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/login_history/controller/LoginHistoryController.java
@@ -1,6 +1,7 @@
 package intbyte4.learnsmate.login_history.controller;
 
 import intbyte4.learnsmate.login_history.domain.dto.LoginHistoryDTO;
+import intbyte4.learnsmate.login_history.domain.vo.request.RequestSaveLoginHistoryVO;
 import intbyte4.learnsmate.login_history.domain.vo.response.ResponseFindLoginHistoryVO;
 import intbyte4.learnsmate.login_history.mapper.LoginHistoryMapper;
 import intbyte4.learnsmate.login_history.service.LoginHistoryService;
@@ -8,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -54,6 +52,18 @@ public class LoginHistoryController {
                 .body(loginHistoryMapper.fromLoginHistoryDTOToResponseFindLoginHistoryVO(loginHistoryDTOList));
     }
 
+    // 4. 로그인 내역에 추가 -> 어떤 회원이 로그인 -> 로그인 시간을 알고있음. -> 로그아웃 시간만 비어있는 상태로 진행하기 -> 로그인 내역 번호를 알고있어야함. -> 반환해주기
+    @PostMapping("/member")
+    public ResponseEntity<Long> saveLoginHisotry(
+            @RequestBody RequestSaveLoginHistoryVO request){
+
+        Long loginHistoryCode =
+                loginHistoryService.saveLoginHistory(loginHistoryMapper.fromRequestSaveLoginHistoryVOtoLoginHistoryDTO(request));
+
+        // 로그인 내역 코드 반환해주기
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(loginHistoryCode);
+    }
 
 
 
--- LearnsMate/src/main/java/intbyte4/learnsmate/login_history/service/LoginHistoryService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/login_history/service/LoginHistoryService.java
@@ -6,6 +6,10 @@ import intbyte4.learnsmate.login_history.domain.dto.LoginHistoryDTO;
 import intbyte4.learnsmate.login_history.domain.entity.LoginHistory;
 import intbyte4.learnsmate.login_history.mapper.LoginHistoryMapper;
 import intbyte4.learnsmate.login_history.repository.LoginHistoryRepository;
+import intbyte4.learnsmate.member.domain.dto.MemberDTO;
+import intbyte4.learnsmate.member.domain.entity.Member;
+import intbyte4.learnsmate.member.mapper.MemberMapper;
+import intbyte4.learnsmate.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,8 @@ public class LoginHistoryService {
 
     private final LoginHistoryRepository loginHistoryRepository;
     private final LoginHistoryMapper loginHistoryMapper;
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
 
     // 모든 로그인 내역 조회 (List)
     public List<LoginHistoryDTO> findAllLoginHistory() {
@@ -46,5 +52,20 @@ public class LoginHistoryService {
         }
 
         return loginHistoryMapper.fromLoginHistoryToLoginHistoryDTO(loginHistoryList);
+    }
+
+    // 특정 멤버가 로그인 할시 로그인 내역에 저장하는 메서드(로그아웃 시간은 없는 경우)
+    public Long saveLoginHistory(LoginHistoryDTO loginHistoryDTO) {
+
+        MemberDTO memberDTO = memberService.findById(loginHistoryDTO.getMemberCode());
+        Member member = memberMapper.fromMemberDTOtoMember(memberDTO);
+
+        LoginHistory loginHistory =
+                loginHistoryMapper.fromLoginHistoryDTOtoLoginHistory(loginHistoryDTO, member);
+
+        loginHistoryRepository.save(loginHistory);
+
+        // 로그인 내역 코드 반환 -> 로그아웃 시간 저장 해야하기 때문
+        return loginHistory.getLoginHistoryCode();
     }
 }
