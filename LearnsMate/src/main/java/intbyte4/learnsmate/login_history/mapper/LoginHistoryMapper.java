8cb3111de6eb770033458d2f0797d3fcb5b0d6dd
Hellin22 <dno06117@naver.com>
2024-11-10 15:28:01 +0900

    ✨ 기능 추가 / 수정: VO <-> DTO <-> ENTITY 매퍼 클래스 작성

--- LearnsMate/src/main/java/intbyte4/learnsmate/login_history/mapper/LoginHistoryMapper.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/login_history/mapper/LoginHistoryMapper.java
@@ -1,7 +1,57 @@
 package intbyte4.learnsmate.login_history.mapper;
 
+import intbyte4.learnsmate.login_history.domain.dto.LoginHistoryDTO;
+import intbyte4.learnsmate.login_history.domain.entity.LoginHistory;
+import intbyte4.learnsmate.login_history.domain.vo.response.ResponseFindLoginHistoryVO;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class LoginHistoryMapper {
+
+    // 리스트
+    public List<LoginHistoryDTO> fromLoginHistoryToLoginHistoryDTO(List<LoginHistory> loginHistoryList) {
+        return loginHistoryList.stream()
+                .map(entity -> LoginHistoryDTO.builder()
+                        .loginHistoryCode(entity.getLoginHistoryCode())
+                        .lastLoginDate(entity.getLastLoginDate())
+                        .lastLogoutDate(entity.getLastLogoutDate())
+                        .memberCode(entity.getMember().getMemberCode())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    // 1개
+    public LoginHistoryDTO fromLoginHistoryToLoginHistoryDTO(LoginHistory loginHistory) {
+        return LoginHistoryDTO.builder()
+                .loginHistoryCode(loginHistory.getLoginHistoryCode())
+                .lastLoginDate(loginHistory.getLastLoginDate())
+                .lastLogoutDate(loginHistory.getLastLogoutDate())
+                .memberCode(loginHistory.getMember().getMemberCode())
+                .build();
+    }
+
+    // 리스트
+    public List<ResponseFindLoginHistoryVO> fromLoginHistoryDTOToResponseFindLoginHistoryVO(List<LoginHistoryDTO> loginHistoryDTOList) {
+        return loginHistoryDTOList.stream()
+                .map(dto -> ResponseFindLoginHistoryVO.builder()
+                        .loginHistoryCode(dto.getLoginHistoryCode())
+                        .lastLoginDate(dto.getLastLoginDate())
+                        .lastLogoutDate(dto.getLastLogoutDate())
+                        .memberCode(dto.getMemberCode())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    // 1개
+    public ResponseFindLoginHistoryVO fromLoginHistoryDTOToResponseFindLoginHistoryVO(LoginHistoryDTO loginHistoryDTO) {
+        return ResponseFindLoginHistoryVO.builder()
+                .loginHistoryCode(loginHistoryDTO.getLoginHistoryCode())
+                .lastLoginDate(loginHistoryDTO.getLastLoginDate())
+                .lastLogoutDate(loginHistoryDTO.getLastLogoutDate())
+                .memberCode(loginHistoryDTO.getMemberCode())
+                .build();
+    }
 }
