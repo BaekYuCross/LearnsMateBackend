e5ffe5f756fef7b8bccdd04b38fe73a74f5ab26f
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-10 19:16:20 +0900

    ✨ 기능 추가 : VideoByLectureMapper dto <-> entity 기능 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/video_by_lecture/mapper/VideoByLectureMapper.java
@@ -0,0 +1,32 @@
+package intbyte4.learnsmate.video_by_lecture.mapper;
+
+import intbyte4.learnsmate.lecture.domain.entity.Lecture;
+import intbyte4.learnsmate.video_by_lecture.domain.dto.VideoByLectureDTO;
+import intbyte4.learnsmate.video_by_lecture.domain.entity.VideoByLecture;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VideoByLectureMapper {
+
+
+    // Entity -> DTO 변환
+    public VideoByLectureDTO toDTO(VideoByLecture videoByLecture) {
+        return VideoByLectureDTO.builder()
+                .videoCode(videoByLecture.getVideoCode())
+                .videoLink(videoByLecture.getVideoLink())
+                .videoTitle(videoByLecture.getVideoTitle())
+                .lectureCode(videoByLecture.getLecture().getLectureCode())
+                .build();
+    }
+
+    // DTO -> Entity 변환
+    public VideoByLecture toEntity(VideoByLectureDTO videoByLectureDTO, Lecture lecture) {
+        return VideoByLecture.builder()
+                .videoCode(videoByLectureDTO.getVideoCode())
+                .videoLink(videoByLectureDTO.getVideoLink())
+                .videoTitle(videoByLectureDTO.getVideoTitle())
+                .lecture(lecture)
+                .build();
+    }
+
+}
