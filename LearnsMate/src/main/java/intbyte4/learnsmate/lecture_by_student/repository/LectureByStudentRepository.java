87ab302d4f44c5e2233308f4f4ef3af569f53035
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-10 21:43:51 +0900

    ✨ 기능 추가 : 학생별 모든 강의 조회 (refund_status 가 true인것만) 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture_by_student/repository/LectureByStudentRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture_by_student/repository/LectureByStudentRepository.java
@@ -1,11 +1,14 @@
 package intbyte4.learnsmate.lecture_by_student.repository;
 
 import intbyte4.learnsmate.lecture_by_student.domain.entity.LectureByStudent;
+import intbyte4.learnsmate.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface LectureByStudentRepository extends JpaRepository<LectureByStudent, Long> {
 
-//    List<LectureByStudent> findByStudentCode(Long memberCode);
+    List<LectureByStudent> findByStudentAndRefundStatus(Member student, boolean refundStatus);
 }
--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture_by_student/service/LectureByStudentServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture_by_student/service/LectureByStudentServiceImpl.java
@@ -1,13 +1,21 @@
 package intbyte4.learnsmate.lecture_by_student.service;
 
+import intbyte4.learnsmate.common.exception.CommonException;
+import intbyte4.learnsmate.common.exception.StatusEnum;
 import intbyte4.learnsmate.lecture_by_student.domain.dto.LectureByStudentDTO;
+import intbyte4.learnsmate.lecture_by_student.domain.entity.LectureByStudent;
 import intbyte4.learnsmate.lecture_by_student.mapper.LectureByStudentMapper;
 import intbyte4.learnsmate.lecture_by_student.repository.LectureByStudentRepository;
+import intbyte4.learnsmate.member.domain.MemberType;
+import intbyte4.learnsmate.member.domain.dto.MemberDTO;
+import intbyte4.learnsmate.member.domain.entity.Member;
+import intbyte4.learnsmate.member.mapper.MemberMapper;
 import intbyte4.learnsmate.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,22 +24,30 @@ public class LectureByStudentServiceImpl implements LectureByStudentService {
     private final LectureByStudentRepository lectureByStudentRepository;
     private final LectureByStudentMapper lectureByStudentMapper;
     private final MemberService memberService;
+    private final MemberMapper memberMapper;
 
-    // 학생별 모든 강의 조회
+    // 학생별 모든 강의 조회 (refund_status 가 true인것만)
     @Override
-    public List<LectureByStudentDTO> findByStudentCode(Long lectureByStudentCode) {
-//        MemberDTO student = memberService.findByMemberCode(lectureByStudentCode);
-//        List<LectureByStudent> lecturesByStudent = lectureByStudentRepository.findByStudentCode(student.getMemberCode());
-//
-//        if (lecturesByStudent.isEmpty()) {
-//            throw new CommonException(StatusEnum.LECTURE_NOT_FOUND);
-//        }
-//
-//        return lecturesByStudent.stream()
-//                .map(lectureByStudentMapper::toDTO)
-//                .collect(Collectors.toList());
-        return null;
+    public List<LectureByStudentDTO> findByStudentCode(Long studentCode) {
+
+        MemberDTO studentDTO = memberService.findMemberByMemberCode(studentCode, MemberType.STUDENT);
+        Member student = memberMapper.fromMemberDTOtoMember(studentDTO);
+
+        // 환불 상태가 true인 강의만 조회
+        List<LectureByStudent> lecturesByStudent = lectureByStudentRepository.findByStudentAndRefundStatus(student, true);
+
+        if (lecturesByStudent.isEmpty()) {
+            throw new CommonException(StatusEnum.LECTURE_NOT_FOUND);
+        }
+
+        return lecturesByStudent.stream()
+                .map(lectureByStudentMapper::toDTO)
+                .collect(Collectors.toList());
     }
+
+
+    // 강의별 학생코드 개수 조회 refund_status 가 true인것만
+
 
 
 }
