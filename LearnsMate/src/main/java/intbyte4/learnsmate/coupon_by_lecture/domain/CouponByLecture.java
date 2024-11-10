ea630228851bb85a01a7aa8d1a5877af4c08667d
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-10 23:54:17 +0900

    ✨ 기능 추가 : 강의 별 쿠폰 엔터티 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/coupon_by_lecture/domain/CouponByLecture.java
@@ -0,0 +1,32 @@
+package intbyte4.learnsmate.coupon_by_lecture.domain;
+
+import intbyte4.learnsmate.coupon.domain.entity.CouponEntity;
+import intbyte4.learnsmate.lecture.domain.entity.Lecture;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "couponByLecture")
+@Table(name = "coupon_by_lecture")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Builder
+public class CouponByLecture {
+
+    @Id
+    @Column(name = "coupon_by_lecture_code", nullable = false)
+    private Long couponByLectureCode;
+
+    @ManyToOne
+    @JoinColumn(name = "coupon_code")
+    private CouponEntity couponCode;
+
+    @ManyToOne
+    @JoinColumn(name = "lecture_code")
+    private Lecture lectureCode;
+
+    public void updateCouponDetails(CouponEntity coupon) {
+        this.couponCode = coupon;
+    }
+}
