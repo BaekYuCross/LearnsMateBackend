840d6117069a75b9705c5b29f563748f9e2623a7
Hellin22 <dno06117@naver.com>
2024-11-10 15:28:29 +0900

    ✨ 기능 추가 / 수정: LoginHistoryService 전체조회, 코드로 조회하는 메서드 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/login_history/service/LoginHistoryService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/login_history/service/LoginHistoryService.java
@@ -1,9 +1,15 @@
 package intbyte4.learnsmate.login_history.service;
 
+import intbyte4.learnsmate.common.exception.CommonException;
+import intbyte4.learnsmate.common.exception.StatusEnum;
+import intbyte4.learnsmate.login_history.domain.dto.LoginHistoryDTO;
+import intbyte4.learnsmate.login_history.domain.entity.LoginHistory;
 import intbyte4.learnsmate.login_history.mapper.LoginHistoryMapper;
 import intbyte4.learnsmate.login_history.repository.LoginHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +17,23 @@ public class LoginHistoryService {
 
     private final LoginHistoryRepository loginHistoryRepository;
     private final LoginHistoryMapper loginHistoryMapper;
+
+    // 모든 로그인 내역 조회
+    public List<LoginHistoryDTO> findAllLoginHistory() {
+        List<LoginHistory> LoginHistoryList = loginHistoryRepository.findAll();
+
+        // List를 통째로 넘겨주기
+        return loginHistoryMapper
+                .fromLoginHistoryToLoginHistoryDTO(LoginHistoryList);
+    }
+
+    // 1개의 로그인 내역 조회
+    public LoginHistoryDTO findById(Long loginHistoryCode) {
+        LoginHistory loginHistory = loginHistoryRepository.findById(loginHistoryCode)
+                .orElseThrow(() -> new CommonException(StatusEnum.LOGIN_HISTORY_NOT_FOUND));
+
+        return loginHistoryMapper.fromLoginHistoryToLoginHistoryDTO(loginHistory);
+    }
+
+
 }
