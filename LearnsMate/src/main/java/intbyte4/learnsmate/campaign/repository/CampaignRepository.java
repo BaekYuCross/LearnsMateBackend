f2bf57681ab0d23bc300efb79c94cd7ddc18897c
YuJeeun <dbwpdms1122@naver.com>
2024-11-11 17:22:29 +0900

    ✨ 기능 추가: CampaignRepository -> @Primary 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/repository/CampaignRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/repository/CampaignRepository.java
@@ -1,12 +1,12 @@
 package intbyte4.learnsmate.campaign.repository;
 
 import intbyte4.learnsmate.campaign.domain.entity.Campaign;
-import intbyte4.learnsmate.campaign.domain.entity.CampaignTypeEnum;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
+@Primary
 public interface CampaignRepository extends JpaRepository<Campaign, Long>, CampaignRepositoryCustom {
 }
