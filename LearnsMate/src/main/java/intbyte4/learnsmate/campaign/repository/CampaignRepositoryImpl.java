42224d09573c61b91e41529065678e38cdb85853
YuJeeun <dbwpdms1122@naver.com>
2024-11-11 15:56:53 +0900

    ✨ 기능 추가: CampaignRepositoryImpl -> searchBy 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/repository/CampaignRepositoryImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/repository/CampaignRepositoryImpl.java
@@ -1,16 +1,94 @@
 package intbyte4.learnsmate.campaign.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import intbyte4.learnsmate.campaign.domain.entity.Campaign;
+import intbyte4.learnsmate.campaign.domain.entity.QCampaign;
+import intbyte4.learnsmate.common.exception.CommonException;
+import intbyte4.learnsmate.common.exception.StatusEnum;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
-public class CampaignRepositoryImpl implements CampaignRepositoryCustom{
-//    private final JPAQueryFactory queryFactory;
+public class CampaignRepositoryImpl implements CampaignRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+    private final QCampaign qCampaign;
 
     @Override
-    public List<Campaign> findAll() {
-        return null;
+    public List<Campaign> searchBy(Campaign campaign, LocalDateTime startDate, LocalDateTime endDate) {
+
+        return queryFactory
+                .selectFrom(qCampaign)
+                .where(searchByType(campaign)
+                        .and(searchByTitle(campaign))
+                        .and(searchByPeriod(campaign, startDate, endDate))
+                        .and(searchBySentStatus(campaign))
+                        .and(searchByScheduledStatus(campaign))
+                )
+                .fetch();
+    }
+
+    public BooleanBuilder searchByType(Campaign campaign) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        if (campaign.getCampaignType() != null) {
+            booleanBuilder.and(qCampaign.campaignType.eq(campaign.getCampaignType()));
+        }
+
+        return booleanBuilder;
+    }
+
+    public BooleanBuilder searchByTitle(Campaign campaign) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        if (campaign.getCampaignTitle() == null) {
+            return booleanBuilder;
+        }
+
+        String[] keywords = campaign.getCampaignTitle().split(" ");
+        if (keywords.length < 2) {
+            throw new CommonException(StatusEnum.MINIMUM_KEYWORD_LENGTH_REQUIRED);
+        }
+
+        for (String keyword : keywords) {
+            booleanBuilder.or(qCampaign.campaignTitle.containsIgnoreCase(keyword));
+        }
+
+        return booleanBuilder;
+    }
+
+    public BooleanBuilder searchByPeriod(Campaign campaign, LocalDateTime startDate, LocalDateTime endDate) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        if (campaign.getCampaignSendDate() == null) return booleanBuilder;
+
+        if (startDate != null) {
+            booleanBuilder.and(qCampaign.campaignSendDate.goe(startDate));
+        }
+
+        if (endDate != null) {
+            booleanBuilder.and(qCampaign.campaignSendDate.loe(endDate));
+        }
+
+        return booleanBuilder;
+    }
+
+    public BooleanBuilder searchBySentStatus(Campaign campaign) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        if (campaign.getCampaignSendDate() == null) return booleanBuilder;
+
+        booleanBuilder.and(qCampaign.campaignSendDate.loe(LocalDateTime.now()));
+
+        return booleanBuilder;
+    }
+
+    public BooleanBuilder searchByScheduledStatus(Campaign campaign) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        if (campaign.getCampaignSendDate() == null) return booleanBuilder;
+
+        booleanBuilder.and(qCampaign.campaignSendDate.goe(LocalDateTime.now()));
+
+        return booleanBuilder;
     }
 }
