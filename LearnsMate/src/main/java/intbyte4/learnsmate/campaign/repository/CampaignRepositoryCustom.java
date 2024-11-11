2e988f9e2bb0b7f0d0ead53e3d47424c95eb0ea2
YuJeeun <dbwpdms1122@naver.com>
2024-11-11 15:55:25 +0900

    🔥 코드 삭제: CampaignRepositoryCustom -> findAll 삭제

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/repository/CampaignRepositoryCustom.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/repository/CampaignRepositoryCustom.java
@@ -2,8 +2,9 @@ package intbyte4.learnsmate.campaign.repository;
 
 import intbyte4.learnsmate.campaign.domain.entity.Campaign;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface CampaignRepositoryCustom {
-    List<Campaign> findAll();
+    List<Campaign> searchBy(Campaign campaign, LocalDateTime startDate, LocalDateTime endDate);
 }
