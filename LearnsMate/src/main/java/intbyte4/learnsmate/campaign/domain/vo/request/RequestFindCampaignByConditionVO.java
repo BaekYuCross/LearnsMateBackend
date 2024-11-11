e1a18c2c6e13da526a09647391c98d554a61478f
YuJeeun <dbwpdms1122@naver.com>
2024-11-11 17:21:22 +0900

    ✨ 기능 추가: RequestFindCampaignByConditionVO 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/vo/request/RequestFindCampaignByConditionVO.java
@@ -0,0 +1,42 @@
+package intbyte4.learnsmate.campaign.domain.vo.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class RequestFindCampaignByConditionVO {
+    @JsonProperty("campaign_code")
+    private Long campaignCode;
+
+    @JsonProperty("campaign_title")
+    private String campaignTitle;
+
+    @JsonProperty("campaign_contents")
+    private String campaignContents;
+
+    @JsonProperty("campaign_type")
+    private String campaignType;
+
+    @JsonProperty("campaign_send_date")
+    private LocalDateTime campaignSendDate;
+
+    @JsonProperty("created_at")
+    private LocalDateTime createdAt;
+
+    @JsonProperty("updated_at")
+    private LocalDateTime updatedAt;
+
+    @JsonProperty("admin_code")
+    private Long adminCode;
+
+    @JsonProperty("tutor_code")
+    private Long tutorCode;
+}
