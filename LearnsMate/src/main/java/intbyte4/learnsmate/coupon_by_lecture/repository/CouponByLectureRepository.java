016d11395012b4d6955470dcd8194ab8ebe23818
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-11 09:32:27 +0900

    ✨ 기능 추가 : 강의 별 쿠폰 전체 조회 Repository 코드 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/coupon_by_lecture/repository/CouponByLectureRepository.java
@@ -0,0 +1,21 @@
+package intbyte4.learnsmate.coupon_by_lecture.repository;
+
+import intbyte4.learnsmate.coupon.domain.entity.CouponEntity;
+import intbyte4.learnsmate.coupon_by_lecture.domain.CouponByLecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository("couponByLectureRepository")
+public interface CouponByLectureRepository extends JpaRepository<CouponByLecture, Long> {
+
+    CouponByLecture findByCoupon(CouponEntity coupon);
+
+    @Query("SELECT cbl FROM couponByLecture cbl " +
+            "JOIN cbl.lecture l " +
+            "WHERE l.tutorCode.memberCode = :tutorCode")
+    List<CouponByLecture> findByTutorCode(@Param("tutorCode") Long tutorCode);
+}
