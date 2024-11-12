281943afaf6ad84408167f4a76ca5e08d4e9ac6e
YuJeeun <dbwpdms1122@naver.com>
2024-11-12 10:12:16 +0900

    ✨ 기능 추가: LectureByStudentMapper -> toEntity 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture_by_student/mapper/LectureByStudentMapper.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture_by_student/mapper/LectureByStudentMapper.java
@@ -1,8 +1,13 @@
 package intbyte4.learnsmate.lecture_by_student.mapper;
 
 
+import intbyte4.learnsmate.issue_coupon.domain.IssueCoupon;
+import intbyte4.learnsmate.lecture.domain.entity.Lecture;
 import intbyte4.learnsmate.lecture_by_student.domain.dto.LectureByStudentDTO;
 import intbyte4.learnsmate.lecture_by_student.domain.entity.LectureByStudent;
+import intbyte4.learnsmate.member.domain.entity.Member;
+import intbyte4.learnsmate.payment.domain.dto.PaymentDTO;
+import intbyte4.learnsmate.payment.domain.entity.Payment;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +19,15 @@ public class LectureByStudentMapper {
                 .refundStatus(entity.getRefundStatus())
                 .lecture(entity.getLecture())
                 .student(entity.getStudent())
+                .build();
+    }
+
+    public LectureByStudent toEntity(LectureByStudentDTO lectureByStudentDTO, Lecture lecture, Member student) {
+        return LectureByStudent.builder()
+                .lectureByStudentCode(lectureByStudentDTO.getLectureByStudentCode())
+                .refundStatus(lectureByStudentDTO.getRefundStatus())
+                .lecture(lecture)
+                .student(student)
                 .build();
     }
 
