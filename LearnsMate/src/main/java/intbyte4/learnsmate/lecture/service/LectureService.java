9ec152b0a49bdb4f4e2f6a00d7bda69ab42f968a
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-12 17:46:39 +0900

    ✨ 기능 추가 : LectureService <=> LectureFacade 코드 리팩 토링 및 DTO 통합 반환 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/service/LectureService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/service/LectureService.java
@@ -1,6 +1,5 @@
 package intbyte4.learnsmate.lecture.service;
 
-import intbyte4.learnsmate.issue_coupon.domain.dto.IssueCouponDTO;
 import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
 
 import java.util.List;
@@ -8,8 +7,6 @@ import java.util.List;
 public interface LectureService {
     List<LectureDTO> getAllLecture();
     LectureDTO getLectureById(Long lectureCode);
-    List<LectureDTO> getLecturesByTutorCode(Long tutorCode);
-    LectureDTO registerLecture(LectureDTO lectureDTO, List<Integer> lectureCategoryCodeList);
     LectureDTO updateLecture(LectureDTO lectureDTO);
     LectureDTO removeLecture(Long lectureCode);
 }
