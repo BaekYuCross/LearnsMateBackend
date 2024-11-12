3e691422371cfdebbca4f4590a5be09c71efc689
YuJeeun <dbwpdms1122@naver.com>
2024-11-12 16:09:27 +0900

    🔥 코드 삭제: LectureCategoryService -> findByLectureCode 삭제

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture_category/service/LectureCategoryService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture_category/service/LectureCategoryService.java
@@ -4,6 +4,4 @@ import intbyte4.learnsmate.lecture_category.domain.dto.LectureCategoryDTO;
 
 public interface LectureCategoryService {
     LectureCategoryDTO findByLectureCategoryCode(Integer lectureCategoryCode);
-
-    LectureCategoryDTO findByLectureCode(Long lectureCode);
 }
