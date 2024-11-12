b6326cd79acfcf754bb342760a5efce1904a5d9d
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-12 20:13:31 +0900

    ✨ 기능 추가 : PaymentDetail 반환 vo 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/payment/domain/vo/ResponseFindPaymentVO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/payment/domain/vo/ResponseFindPaymentVO.java
@@ -2,6 +2,7 @@ package intbyte4.learnsmate.payment.domain.vo;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import intbyte4.learnsmate.lecture.enums.LectureLevelEnum;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +16,20 @@ import java.time.LocalDateTime;
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ResponseFindPaymentVO {
-
     private Long paymentCode;
     private Integer paymentPrice;
     private LocalDateTime createdAt;
-    private Long lectureByStudentCode;
-    private String couponIssuanceCode;
+    private Long lectureCode; // 강의코드
+    private String lectureTitle; // 강의명
+    private Integer lecturePrice; // 강의 원가
+    private Long tutorCode; // 강사 코드
+    private String tutorName; // 강사 명
+    private Long studentCode; // 학생 코드
+    private String studentName; // 학생 명
+    private Boolean lectureStatus; // 강의 상태
+    private String lectureCategory; // 강의 카테고리
+    private Integer lectureClickCount; // 강의 조회수
+    private LectureLevelEnum lectureLevel; // 강의 난이도
+    private String couponIssuanceCode; // 적용 쿠폰 코드
+    private String couponIssuanceName; // 적용 쿠폰명
 }
