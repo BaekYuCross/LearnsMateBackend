85312297596537e8f4f44fddf69cc9dc8679e48d
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-13 17:10:41 +0900

    ✨ 기능 추가 : toAcceptConfirmStatus true 변환 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/entity/Lecture.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/domain/entity/Lecture.java
@@ -2,7 +2,6 @@ package intbyte4.learnsmate.lecture.domain.entity;
 
 import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
 import intbyte4.learnsmate.lecture.enums.LectureLevelEnum;
-import intbyte4.learnsmate.lecture_category.domain.entity.LectureCategory;
 import intbyte4.learnsmate.member.domain.entity.Member;
 import jakarta.persistence.*;
 import lombok.*;
@@ -70,6 +69,11 @@ public class Lecture {
 
     public void toDelete(){
         this.lectureStatus = false;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void toAcceptConfirmStatus(){
+        this.lectureConfirmStatus = true;
         this.updatedAt = LocalDateTime.now();
     }
 
