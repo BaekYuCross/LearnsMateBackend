7c85af36e6d522bf9c20e57f9e82769fe4083bf5
YuJeeun <dbwpdms1122@naver.com>
2024-11-13 11:28:10 +0900

    ♻️ 코드 수정: 불필요한 코드 삭제

--- LearnsMate/src/main/java/intbyte4/learnsmate/facade/LectureFacade.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/facade/LectureFacade.java
@@ -67,9 +67,6 @@ public class LectureFacade {
 
         Lecture lecture = lectureMapper.toEntity(lectureDTO, tutor);
 
-        MemberDTO studentDTO = memberService.findById(issueCouponDTO.getStudentCode());
-        Member student = memberMapper.fromMemberDTOtoMember(studentDTO);
-
         CouponDTO couponDTO = couponService.findCouponDTOByCouponCode(issueCouponDTO.getCouponCode());
         // 쿠폰카테고리디티오로 반환하는게 필요
         CouponCategory couponCategory = couponCategoryService.findByCouponCategoryCode(couponDTO.getCouponCategoryCode());
