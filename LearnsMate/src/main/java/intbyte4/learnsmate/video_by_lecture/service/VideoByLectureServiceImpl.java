edeeca9850e4fdc1ad685193d27a54865b5b3992
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-13 14:39:47 +0900

    ♻️ 코드 수정 : PaymentFacade 패키지명 수정

--- LearnsMate/src/main/java/intbyte4/learnsmate/video_by_lecture/service/VideoByLectureServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/video_by_lecture/service/VideoByLectureServiceImpl.java
@@ -2,18 +2,15 @@ package intbyte4.learnsmate.video_by_lecture.service;
 
 import intbyte4.learnsmate.common.exception.CommonException;
 import intbyte4.learnsmate.common.exception.StatusEnum;
-import intbyte4.learnsmate.facade.LectureFacade;
 import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
 import intbyte4.learnsmate.lecture.domain.entity.Lecture;
 import intbyte4.learnsmate.lecture.mapper.LectureMapper;
 import intbyte4.learnsmate.lecture.service.LectureService;
-import intbyte4.learnsmate.lecture_by_student.service.LectureByStudentService;
 import intbyte4.learnsmate.member.domain.MemberType;
 import intbyte4.learnsmate.member.domain.dto.MemberDTO;
 import intbyte4.learnsmate.member.domain.entity.Member;
 import intbyte4.learnsmate.member.mapper.MemberMapper;
 import intbyte4.learnsmate.member.service.MemberService;
-import intbyte4.learnsmate.video_by_lecture.domain.dto.CountVideoByLectureDTO;
 import intbyte4.learnsmate.video_by_lecture.domain.dto.VideoByLectureDTO;
 import intbyte4.learnsmate.video_by_lecture.domain.entity.VideoByLecture;
 import intbyte4.learnsmate.video_by_lecture.mapper.VideoByLectureMapper;
@@ -21,7 +18,6 @@ import intbyte4.learnsmate.video_by_lecture.repository.VideoByLectureRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
