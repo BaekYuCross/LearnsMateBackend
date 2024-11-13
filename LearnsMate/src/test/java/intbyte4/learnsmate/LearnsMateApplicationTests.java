e1482b342a295be18242af8609c1ed58d593be1d
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-13 16:04:55 +0900

    ♻️ 코드 수정 : application-test가 활성화되도록 수정

--- LearnsMate/src/test/java/intbyte4/learnsmate/LearnsMateApplicationTests.java
+++ LearnsMate/src/test/java/intbyte4/learnsmate/LearnsMateApplicationTests.java
@@ -2,8 +2,10 @@ package intbyte4.learnsmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LearnsMateApplicationTests {
 
     @Test
