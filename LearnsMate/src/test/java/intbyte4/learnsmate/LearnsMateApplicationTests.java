589c2992f5041873da596e22ce722b78fe66466b
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-13 16:20:10 +0900

    ♻️ 코드 수정 : deploy.yml 수정

--- LearnsMate/src/test/java/intbyte4/learnsmate/LearnsMateApplicationTests.java
+++ LearnsMate/src/test/java/intbyte4/learnsmate/LearnsMateApplicationTests.java
@@ -2,10 +2,8 @@ package intbyte4.learnsmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class LearnsMateApplicationTests {
 
     @Test
