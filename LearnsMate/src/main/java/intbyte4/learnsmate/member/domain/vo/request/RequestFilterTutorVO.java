66b94ef83aedd77895d8b6d55bd0faef4607fe40
Hellin22 <dno06117@naver.com>
2024-11-13 16:15:08 +0900

    ✨ 기능 추가 / 수정: 강사 필터링 vo 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/member/domain/vo/request/RequestFilterTutorVO.java
@@ -0,0 +1,14 @@
+package intbyte4.learnsmate.member.domain.vo.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class RequestFilterTutorVO {
+}
