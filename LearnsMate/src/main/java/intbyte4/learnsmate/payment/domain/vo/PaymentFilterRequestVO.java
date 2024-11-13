e72b613610a2fead8a7708350b9156e39d4d9838
seohyun <694090@gmail.com>
2024-11-13 14:24:15 +0900

    ✨ 기능 추가: PaymentFilterRequestVO 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/payment/domain/vo/PaymentFilterRequestVO.java
@@ -0,0 +1,57 @@
+package intbyte4.learnsmate.payment.domain.vo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import intbyte4.learnsmate.lecture.enums.LectureLevelEnum;
+
+import java.time.LocalDateTime;
+
+public class PaymentFilterRequestVO {
+
+    @JsonProperty("payment_code")
+    private Long paymentCode;
+
+    @JsonProperty("min_payment_price")
+    private Integer minPaymentPrice;
+
+    @JsonProperty("max_payment_price")
+    private Integer maxPaymentPrice;
+
+    @JsonProperty("start_created_at")
+    private LocalDateTime startCreatedAt;
+
+    @JsonProperty("end_created_at")
+    private LocalDateTime endCreatedAt;
+
+    @JsonProperty("leture_code")
+    private Long lectureCode; // 강의코드
+
+    @JsonProperty("lecture_title")
+    private String lectureTitle; // 강의명
+
+    @JsonProperty("min_lecture_price")
+    private Integer minLecturePrice; // 강의 원가
+
+    @JsonProperty("max_lecture_price")
+    private Integer maxLecturePrice;
+
+    @JsonProperty("tutor_code")
+    private Long tutorCode; // 강사 코드
+
+    @JsonProperty("tutor_name")
+    private String tutorName; // 강사명
+
+    @JsonProperty("student_code")
+    private Long studentCode; // 학생 코드
+
+    @JsonProperty("student_name")
+    private String studentName; // 학생명
+
+    @JsonProperty("lecture_category")
+    private String lectureCategory; // 강의 카테고리
+
+    @JsonProperty("coupon_issuance_code")
+    private String couponIssuanceCode; // 적용 쿠폰 코드
+
+    @JsonProperty("coupon_issuance_name")
+    private String couponIssuanceName; // 적용 쿠폰명
+}
