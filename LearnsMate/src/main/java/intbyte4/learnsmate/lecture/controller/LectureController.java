99046624ab41983a179680e3a4186bd661a0084b
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-15 20:13:46 +0900

    ✨ 기능 추가 : 강의 추가, 수정 시 메서드 수정 및 월별/연도별 컨트롤러 코드 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/controller/LectureController.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/controller/LectureController.java
@@ -1,5 +1,6 @@
 package intbyte4.learnsmate.lecture.controller;
 
+import intbyte4.learnsmate.lecture.domain.dto.MonthlyLectureCountDTO;
 import intbyte4.learnsmate.lecture.service.LectureFacade;
 import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
 import intbyte4.learnsmate.lecture.domain.dto.LectureDetailDTO;
@@ -33,10 +34,10 @@ public class LectureController {
     private final LectureFacade lectureFacade;
 
     @Operation(summary = "학생이 강의를 클릭할 때 클릭 수 증가")
-    @PostMapping("/{lectureCode}/click")
-    public ResponseEntity<Void> incrementClickCount(@PathVariable("lectureCode") Long lectureCode) {
+    @PatchMapping("/{lectureCode}/click")
+    public ResponseEntity<?> incrementClickCount(@PathVariable("lectureCode") String lectureCode) {
         try {
-            lectureService.getLecturesByStudentCode(lectureCode);
+            lectureService.incrementClickCount(lectureCode);
             return ResponseEntity.status(HttpStatus.OK).build();
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -55,51 +56,46 @@ public class LectureController {
 
     @Operation(summary = "강의 단건 조회")
     @GetMapping("/{lectureCode}")
-    public ResponseEntity<ResponseFindLectureVO> getLecture(@PathVariable("lectureCode") Long lectureCode) {
+    public ResponseEntity<ResponseFindLectureVO> getLecture(@PathVariable("lectureCode") String lectureCode) {
         LectureDetailDTO lectureDTO = lectureFacade.getLectureById(lectureCode);
         return ResponseEntity.status(HttpStatus.OK).body(lectureMapper.fromDtoToResponseVO(lectureDTO));
     }
 
-    @Operation(summary = "강의와 강의별 동영상, 강의별 카테고리 및 동영상 등록")
-    @PutMapping("/{lectureCode}")
-    public ResponseEntity<ResponseRegisterLectureVO> registerLecture(
-            @RequestBody RequestRegisterLectureVO registerLectureVO) {
-
+    @Operation(summary = "강의와 강의별 동영상 등록 요청")
+    @PostMapping("/register")
+    public ResponseEntity<ResponseRegisterLectureVO> registerLecture(@RequestBody RequestRegisterLectureVO registerLectureVO) {
         LectureDTO lectureDTO = lectureMapper.fromRegisterRequestVOtoDto(registerLectureVO);
         List<Integer> lectureCategoryCodeList = registerLectureVO.getLectureCategoryCodeList();
         List<VideoByLectureDTO> videoByLectureDTOList = registerLectureVO.getVideoByLectureDTOList();
 
         LectureDTO registeredLectureDTO = lectureFacade.registerLecture(lectureDTO, lectureCategoryCodeList, videoByLectureDTOList);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(lectureMapper.fromDtoToRegisterResponseVO(registeredLectureDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(lectureMapper.fromDtoToRegisterResponseVO(registeredLectureDTO));
     }
-
 
     @Operation(summary = "강의 수정")
     @PatchMapping("/{lectureCode}/info")
-    public ResponseEntity<ResponseEditLectureInfoVO> updateLecture(
-            @RequestBody RequestEditLectureInfoVO requestVO) {
-
+    public ResponseEntity<ResponseEditLectureInfoVO> updateLecture(@RequestBody RequestEditLectureInfoVO requestVO, @PathVariable String lectureCode) {
         LectureDTO lectureDTO = lectureMapper.fromRequestVOtoDto(requestVO);
+        lectureDTO.setLectureCode(lectureCode);
 
-        LectureDTO updatedLecture = lectureFacade.updateLecture(lectureDTO,
-                requestVO.getNewVideoTitle(),
-                requestVO.getNewVideoLink(),
-                requestVO.getLectureCategoryCodeList());
+        LectureDTO updatedLecture = lectureFacade.updateLecture(lectureDTO, requestVO.getNewVideoTitle(), requestVO.getNewVideoLink(), requestVO.getLectureCategoryCodeList());
 
         ResponseEditLectureInfoVO responseVO = lectureMapper.fromDtoToEditResponseVO(updatedLecture);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(responseVO);
     }
 
-
-
     @Operation(summary = "강의 삭제")
     @PatchMapping("/{lectureCode}/status")
-    public ResponseEntity<ResponseRemoveLectureVO> removeLecture(@PathVariable("lectureCode")  Long lectureCode) {
+    public ResponseEntity<ResponseRemoveLectureVO> removeLecture(@PathVariable("lectureCode")  String lectureCode) {
         LectureDTO removedLecture = lectureFacade.removeLecture(lectureCode);
         return ResponseEntity.status(HttpStatus.OK).body(lectureMapper.fromDtoToRemoveResponseVO(removedLecture));
     }
 
+    @Operation(summary = "월별/연도별 전체 강의 수 조회")
+    @GetMapping("/monthly-counts")
+    public ResponseEntity<List<MonthlyLectureCountDTO>> getMonthlyLectureCounts() {
+        List<MonthlyLectureCountDTO> lectureCounts = lectureService.getMonthlyLectureCounts();
+        return ResponseEntity.ok(lectureCounts);
+    }
 }
