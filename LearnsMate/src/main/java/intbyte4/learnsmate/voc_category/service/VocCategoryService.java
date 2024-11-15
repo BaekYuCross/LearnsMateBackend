a2327c7566f8fa62e1a1b1e22ce8f193b102c193
YuJeeun <dbwpdms1122@naver.com>
2024-11-15 19:57:58 +0900

    ✨ 기능 추가: VocCategoryService -> findAll 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/voc_category/service/VocCategoryService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/voc_category/service/VocCategoryService.java
@@ -1,7 +1,12 @@
 package intbyte4.learnsmate.voc_category.service;
 
 import intbyte4.learnsmate.voc_category.domain.VocCategory;
+import intbyte4.learnsmate.voc_category.domain.dto.VocCategoryDTO;
+
+import java.util.List;
 
 public interface VocCategoryService {
     VocCategory findByVocCategoryCode(Integer vocCategoryCode);
+
+    List<VocCategoryDTO> findAll();
 }
--- LearnsMate/src/main/java/intbyte4/learnsmate/voc_category/service/VocCategoryServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/voc_category/service/VocCategoryServiceImpl.java
@@ -1,10 +1,14 @@
 package intbyte4.learnsmate.voc_category.service;
 
 import intbyte4.learnsmate.voc_category.domain.VocCategory;
+import intbyte4.learnsmate.voc_category.domain.dto.VocCategoryDTO;
 import intbyte4.learnsmate.voc_category.repository.VocCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service("vocCategoryService")
@@ -17,5 +21,14 @@ public class VocCategoryServiceImpl implements VocCategoryService {
     public VocCategory findByVocCategoryCode(Integer vocCategoryCode) {
         log.info("vocCategory 조회 중: {}", vocCategoryCode);
         return vocCategoryRepository.findById(vocCategoryCode).orElseThrow(() -> new RuntimeException("vocCategoryCode not found"));
+    }
+
+    public List<VocCategoryDTO> findAll() {
+        return vocCategoryRepository.findAll().stream()
+                .map(vocCategory -> VocCategoryDTO.builder()
+                        .vocCategoryCode(vocCategory.getVocCategoryCode())
+                        .vocCategoryName(vocCategory.getVocCategoryName())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
