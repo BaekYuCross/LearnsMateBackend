dd392f121f3f9be43330b7c2c699f0f04f7de8a4
seohyun <694090@gmail.com>
2024-11-16 23:03:37 +0900

    ♻️ 코드 수정: CouponCategoryEnum 수정

--- LearnsMate/src/main/java/intbyte4/learnsmate/coupon_category/domain/dto/CouponCategoryEnum.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/coupon_category/domain/dto/CouponCategoryEnum.java
@@ -1,26 +1,50 @@
 package intbyte4.learnsmate.coupon_category.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
+@Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum CouponCategoryEnum {
-    COMMON("일반"),
-    ANNIVERSARY("기념일"),
-    NEW("신규가입"),
-    FIRSTPURCHASE("첫구매"),
-    RETURN("복귀"),
-    COMPLETE("완료"),
-    EVENT("이벤트");
+    COMMON(1, "일반"),
+    ANNIVERSARY(2, "기념일"),
+    NEW(3, "신규가입"),
+    FIRSTPURCHASE(4, "첫구매"),
+    RETURN(5, "복귀"),
+    COMPLETE(6, "완료"),
+    EVENT(7, "이벤트");
 
-    private final String displayName;
+    private final Integer categoryCode;
+    private final String categoryName;
 
-    CouponCategoryEnum(String displayName) {
-        this.displayName = displayName;
+    CouponCategoryEnum(Integer categoryCode, String categoryName) {
+        this.categoryCode = categoryCode;
+        this.categoryName = categoryName;
     }
 
-    @JsonValue
-    public String getDisplayName() {
-        return displayName;
+    public Integer getCategoryCode() {
+        return categoryCode;
     }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public static Integer getCodeByName(String categoryName) {
+        for(CouponCategoryEnum category : CouponCategoryEnum.values()) {
+            if (category.getCategoryName().equals(categoryName)) {
+                return category.getCategoryCode();
+            }
+        }
+        throw new IllegalArgumentException("해당 카테고리의 이름이 존재하지 않습니다: " + categoryName);
+    }
+//
+//    public static String getNameByCode(Integer categoryCode) {
+//        for (CouponCategoryEnum category : CouponCategoryEnum.values()) {
+//            if (category.getCategoryCode().equals(categoryCode)) {
+//                return category.getCategoryName();
+//            }
+//        }
+//        throw new IllegalArgumentException("해당 카테고리 코드가 존재하지 않습니다: " + categoryCode);
+//    }
 }
