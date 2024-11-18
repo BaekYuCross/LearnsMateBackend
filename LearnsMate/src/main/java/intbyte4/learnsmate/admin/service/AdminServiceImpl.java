4a1a3467acc7a3b8b7d1228990bee9b029dece3b
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-18 10:26:33 +0900

    ✨ 기능 추가 : loadUserByUsername Service 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/admin/service/AdminServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/admin/service/AdminServiceImpl.java
@@ -8,7 +8,15 @@ import intbyte4.learnsmate.admin.repository.AdminRepository;
 import intbyte4.learnsmate.common.exception.CommonException;
 import intbyte4.learnsmate.common.exception.StatusEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,4 +39,30 @@ public class AdminServiceImpl implements AdminService {
         Admin updatedAdmin = adminRepository.save(admin);
         return adminMapper.toDTO(updatedAdmin);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Admin admin = adminRepository.findByAdminName(username);
+
+        if (admin == null) throw new CommonException(StatusEnum.ADMIN_NOT_FOUND);
+
+
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+//        switch (admin.getUserType()) {
+//            case "Employee" -> grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
+//            case "Employer" -> grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYER"));
+//            case "Admin" -> {
+//                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+//                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYER"));
+//                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
+//            }
+//        }
+        if (!admin.getAdminStatus()) throw new CommonException(StatusEnum.ADMIN_NOT_FOUND);
+        // 고객의 학생과 강사도 가져와서 예외처리랑 역할 분배 해주기
+
+        return new User(admin.getAdminEmail(), admin.getAdminPassword(), true, true, true, true, grantedAuthorities);
+    }
 }
+
