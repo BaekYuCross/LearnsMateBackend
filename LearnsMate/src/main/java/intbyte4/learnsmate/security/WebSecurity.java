2e615482c34b0fadd8f676caa05385e309965e43
seohyun <694090@gmail.com>
2024-11-20 12:11:47 +0900

    ♻️ 코드 수정: admin, tutor 조회 시 발생하는 null pointer exception 해결

--- LearnsMate/src/main/java/intbyte4/learnsmate/security/WebSecurity.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/security/WebSecurity.java
@@ -80,6 +80,7 @@ public class WebSecurity {
                                 .requestMatchers(new AntPathRequestMatcher("/voc/filter", "POST")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/voc/count-by-category", "GET")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/member/**", "GET")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/coupon/**", "GET")).permitAll()
                                 .anyRequest().authenticated()
                 )
                 // UserDetails를 상속받는 Service 계층 + BCrypt 암호화
