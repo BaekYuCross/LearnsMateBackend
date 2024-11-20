ae577588db71bd1861b49d0e7390de88c0bf8001
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-20 12:18:54 +0900

    ✨ 기능 추가 : WebSecurity admin 기본 권한 기능 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/admin/service/AdminServiceImpl.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/admin/service/AdminServiceImpl.java
@@ -8,9 +8,14 @@ import intbyte4.learnsmate.admin.mapper.AdminMapper;
 import intbyte4.learnsmate.admin.repository.AdminRepository;
 import intbyte4.learnsmate.common.exception.CommonException;
 import intbyte4.learnsmate.common.exception.StatusEnum;
+import intbyte4.learnsmate.member.domain.MemberType;
+import intbyte4.learnsmate.member.domain.dto.MemberDTO;
+import intbyte4.learnsmate.member.mapper.MemberMapper;
+import intbyte4.learnsmate.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -25,6 +30,8 @@ public class AdminServiceImpl implements AdminService {
 
     private final AdminRepository adminRepository;
     private final AdminMapper adminMapper;
+    private final MemberMapper memberMapper;
+    private final MemberService memberService;
 
     @Override
     public AdminDTO findByAdminCode(Long adminCode) {
@@ -43,26 +50,22 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         Admin admin = adminRepository.findById(Long.parseLong(username))
                 .orElseThrow(() -> new CommonException(StatusEnum.ADMIN_NOT_FOUND));
 
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
 
-//        switch (admin.getUserType()) {
-//            case "Employee" -> grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
-//            case "Employer" -> grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYER"));
-//            case "Admin" -> {
-//                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-//                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYER"));
-//                grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_EMPLOYEE"));
-//            }
-//        }
-        if (!admin.getAdminStatus()) throw new CommonException(StatusEnum.ADMIN_NOT_FOUND);
-        // 고객의 학생과 강사도 가져와서 예외처리랑 역할 분배 해주기
+        // 기본적으로 ROLE_ADMIN 권한 추가
+        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+
+        if (!admin.getAdminStatus()) {
+            throw new CommonException(StatusEnum.ADMIN_NOT_FOUND);
+        }
 
         AdminDTO adminDTO = adminMapper.toDTO(admin);
-
         return new CustomUserDetails(adminDTO, grantedAuthorities, true, true, true, true);
     }
+
 }
 
