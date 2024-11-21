b5e9e2ae92125f5aedd701c0af755e1b43ac5015
YuJeeun <dbwpdms1122@naver.com>
2024-11-21 21:15:29 +0900

    ✨ 기능 추가: CampaignFilterDTO추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/dto/CampaignFilterDTO.java
@@ -0,0 +1,26 @@
+package intbyte4.learnsmate.campaign.domain.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Builder
+public class CampaignFilterDTO {
+    private Long campaignCode;
+    private String campaignTitle;
+    private String campaignContents;
+    private String campaignType;
+    private LocalDateTime campaignSendDate;
+    private LocalDateTime campaignStartPostDate;
+    private LocalDateTime campaignEndPostDate;
+    private String campaignStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long adminCode;
+
+}
