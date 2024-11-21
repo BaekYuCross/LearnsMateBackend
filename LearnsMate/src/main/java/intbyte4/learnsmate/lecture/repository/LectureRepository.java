d7133d67aaa4b351b9ba72f29cdcd417bc3404d7
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-21 14:02:13 +0900

    ✨ 기능 추가 : 전체 조회 시, 월별 강의 데이터 구하기 위해 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/lecture/repository/LectureRepository.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/lecture/repository/LectureRepository.java
@@ -2,13 +2,26 @@ package intbyte4.learnsmate.lecture.repository;
 
 import intbyte4.learnsmate.lecture.domain.entity.Lecture;
 import intbyte4.learnsmate.member.domain.entity.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, String> , JpaSpecificationExecutor<Lecture> {
     List<Lecture> findAllByTutor(Member tutor);
+
+    @Query("SELECT l FROM lecture l WHERE (:cursor IS NULL OR l.createdAt < :cursor) ORDER BY l.createdAt DESC")
+    List<Lecture> findLecturesByCursor(@Param("cursor") LocalDateTime cursor, Pageable pageable);
+
+    @Query("SELECT CONCAT(YEAR(l.createdAt), '-', LPAD(CAST(MONTH(l.createdAt) AS string), 2, '0')) AS monthYear, COUNT(l) " +
+            "FROM lecture l " +
+            "GROUP BY YEAR(l.createdAt), MONTH(l.createdAt) " +
+            "ORDER BY YEAR(l.createdAt), MONTH(l.createdAt)")
+    List<Object[]> findMonthlyLectureCounts();
 }
