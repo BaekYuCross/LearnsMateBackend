87aae7678d1a06b8e809a12b804ae4f7b68e12b8
seoli <694090@gmail.com>
2024-11-21 19:08:26 +0900

    ♻️ 코드 수정 : 발급된 쿠폰 권한 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/security/WebSecurity.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/security/WebSecurity.java
@@ -85,6 +85,8 @@ public class WebSecurity {
                                 .requestMatchers(new AntPathRequestMatcher("/coupon/**", "GET")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/coupon/**", "POST")).permitAll()
                                 .requestMatchers(new AntPathRequestMatcher("/member/**", "POST")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/issue-coupon/**", "GET")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/issue-coupon/**", "POST")).permitAll()
                                 .anyRequest().authenticated()
                 )
                 // UserDetails를 상속받는 Service 계층 + BCrypt 암호화
