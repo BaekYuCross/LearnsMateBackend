54eed7064a14555fe5551625b00e6afa4eab0b12
Hellin22 <dno06117@naver.com>
2024-11-25 21:43:09 +0900

    ✨기능 추가: 로그인시 반환할 client 데이터 vo 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/member/domain/vo/response/ResponseLoginVO.java
@@ -0,0 +1,18 @@
+package intbyte4.learnsmate.member.domain.vo.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ResponseLoginVO {
+
+    private Long memberCode;
+    private String memberName;
+    private String memberEmail;
+}
