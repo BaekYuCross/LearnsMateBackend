1a3cb47c5a7123ca623c693fe12ffb2e36236406
YuJeeun <dbwpdms1122@naver.com>
2024-11-25 21:30:41 +0900

    ✨기능 추가: FindCampaignTemplateDTO 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign_template/domain/dto/FindCampaignTemplateDTO.java
@@ -0,0 +1,22 @@
+package intbyte4.learnsmate.campaign_template.domain.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+public class FindCampaignTemplateDTO {
+    private Long campaignTemplateCode;
+    private String campaignTemplateTitle;
+    private String campaignTemplateContents;
+    private Boolean campaignTemplateFlag;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Long adminCode;
+    private String adminName;
+}
