aaee418e91ccbb4ab251c90afdbdbb5ba7ad6117
YuJeeun <dbwpdms1122@naver.com>
2024-11-25 16:17:03 +0900

    ✨ 기능 추가: MemberExcelController -> uploadTargetStudentExcel  추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/dto/FindCampaignDTO.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign/domain/dto/FindCampaignDTO.java
@@ -1,11 +1,7 @@
 package intbyte4.learnsmate.campaign.domain.dto;
 
 import intbyte4.learnsmate.coupon.domain.dto.CouponDTO;
-import intbyte4.learnsmate.couponbycampaign.domain.dto.CouponByCampaignDTO;
-import intbyte4.learnsmate.member.domain.MemberType;
 import intbyte4.learnsmate.member.domain.dto.MemberDTO;
-import intbyte4.learnsmate.userpercampaign.domain.dto.UserPerCampaignDTO;
-import intbyte4.learnsmate.userpercampaign.domain.entity.UserPerCampaign;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -37,28 +33,4 @@ public class FindCampaignDTO {
 
     // 첨부된 쿠폰 정보
     private List<CouponDTO> coupons;
-
-//    // 타겟 유저 정보
-//    private Long memberCode;
-//    private MemberType memberType;
-//    private String memberEmail;
-//    private String memberPassword;
-//    private String memberName;
-//    private Integer memberAge;
-//    private String memberPhone;
-//    private String memberAddress;
-//    private LocalDateTime memberBirth;
-//    private Boolean memberFlag;
-//    private Boolean memberDormantStatus;
-//
-//    // 첨부된 쿠폰 정보
-//    private Long couponCode;
-//    private String couponName;
-//    private String couponContents;
-//    private int couponDiscountRate;
-//    private LocalDateTime couponStartDate;
-//    private LocalDateTime couponExpireDate;
-//    private Boolean couponFlag;
-//    private int couponCategoryCode;
-//    private Long tutorCode;
 }
