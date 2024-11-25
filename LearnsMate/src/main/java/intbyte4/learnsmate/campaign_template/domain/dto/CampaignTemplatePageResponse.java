4ef027deee71b8ea99d6c6f03c8c4c1013052514
YuJeeun <dbwpdms1122@naver.com>
2024-11-25 19:21:24 +0900

    ✨기능 추가: CampaignTemplatePageResponse 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign_template/domain/dto/CampaignTemplatePageResponse.java
@@ -0,0 +1,17 @@
+package intbyte4.learnsmate.campaign_template.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.poi.ss.formula.functions.T;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class CampaignTemplatePageResponse<T> {
+    private List<T> content;
+    private long totalElements;
+    private int totalPages;
+    private int currentPage;
+    private int size;
+}
