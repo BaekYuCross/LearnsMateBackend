93ac1c4f5a59daf9b7568d56f35eb5366577d076
Hellin22 <dno06117@naver.com>
2024-11-26 12:46:36 +0900

    ✨기능 수정: client logout dto, vo, login history vo 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/member/domain/vo/request/RequestLogoutVO.java
@@ -0,0 +1,13 @@
+package intbyte4.learnsmate.member.domain.vo.request;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class RequestLogoutVO {
+
+    private Long loginHistoryCode;
+}
