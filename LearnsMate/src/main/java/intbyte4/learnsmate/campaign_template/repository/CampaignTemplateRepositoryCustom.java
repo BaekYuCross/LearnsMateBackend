aa8c6c265a0d02ef6b777e589ab3137512b7d3d1
YuJeeun <dbwpdms1122@naver.com>
2024-11-26 20:54:40 +0900

    ✨기능 추가: CampaignTemplateRepositoryCustom -> searchByWithoutPaging 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/campaign_template/repository/CampaignTemplateRepositoryCustom.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/campaign_template/repository/CampaignTemplateRepositoryCustom.java
@@ -5,6 +5,9 @@ import intbyte4.learnsmate.campaign_template.domain.dto.CampaignTemplateFilterDT
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface CampaignTemplateRepositoryCustom {
     Page<CampaignTemplate> searchBy(CampaignTemplateFilterDTO campaignTemplateFilterDTO, Pageable pageable);
+    List<CampaignTemplate> searchByWithoutPaging(CampaignTemplateFilterDTO request);
 }
