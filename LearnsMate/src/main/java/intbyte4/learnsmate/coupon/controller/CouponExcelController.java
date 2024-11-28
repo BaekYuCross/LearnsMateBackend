a41d4627f2167fd12f97288c8ba845563238efa3
Hellin22 <dno06117@naver.com>
2024-11-28 18:57:54 +0900

    ✨기능 추가: 쿠폰 엑셀 다운로드 컨트롤러 추가

---
+++ LearnsMate/src/main/java/intbyte4/learnsmate/coupon/controller/CouponExcelController.java
@@ -0,0 +1,46 @@
+package intbyte4.learnsmate.coupon.controller;
+
+import intbyte4.learnsmate.coupon.service.CouponExcelService;
+import intbyte4.learnsmate.member.domain.MemberType;
+import intbyte4.learnsmate.member.domain.dto.MemberFilterRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coupon/excel")
+@Slf4j
+@RequiredArgsConstructor
+public class CouponExcelController {
+
+    private final CouponExcelService couponExcelService;
+
+    @PostMapping("/download")
+    @Operation(summary = "쿠폰 엑셀 다운로드", description = "쿠폰 목록을 엑셀 파일로 다운로드합니다.")
+    public void downloadStudentExcel(HttpServletResponse response, @RequestBody(required = false) MemberFilterRequestDTO filterDTO) {
+        try {
+            log.info("Excel download request received");
+
+            if (filterDTO != null) {
+                log.info("Filter DTO parsed: {}", filterDTO);
+//                log.info("Answer status type: {}", filterDTO.getVocAnswerStatus() != null ?
+//                        filterDTO.getVocAnswerStatus().getClass().getName() : "null");
+            }
+            response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            response.setHeader("Content-Disposition", "attachment; filename=\"coupon_data.xlsx\"");
+
+            couponExcelService.exportCouponToExcel(response.getOutputStream(), filterDTO);
+
+            log.info("Excel file successfully written to response output stream.");
+        } catch (Exception e) {
+            log.error("Error during excel download:", e);
+            throw new RuntimeException("Excel download failed", e);
+        }
+    }
+
+}
