55a78252eaccb5e7d32d9e629e920c80de4f1fbc
YuJeeun <dbwpdms1122@naver.com>
2024-11-28 10:58:35 +0900

    ✨기능 추가: CouponService -> findCouponsByCampaignCode 추가

--- LearnsMate/src/main/java/intbyte4/learnsmate/coupon/service/CouponService.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/coupon/service/CouponService.java
@@ -1,15 +1,16 @@
 package intbyte4.learnsmate.coupon.service;
 
 import intbyte4.learnsmate.admin.domain.entity.Admin;
+import intbyte4.learnsmate.campaign.domain.dto.FindCampaignDetailDTO;
 import intbyte4.learnsmate.coupon.domain.dto.CouponDTO;
 import intbyte4.learnsmate.coupon.domain.dto.CouponFilterDTO;
 import intbyte4.learnsmate.coupon.domain.entity.CouponEntity;
 import intbyte4.learnsmate.coupon.domain.vo.request.AdminCouponRegisterRequestVO;
-import intbyte4.learnsmate.coupon.domain.vo.request.CouponFilterRequestVO;
-import intbyte4.learnsmate.coupon_category.domain.CouponCategory;
 import intbyte4.learnsmate.lecture.domain.dto.LectureDTO;
 import intbyte4.learnsmate.member.domain.entity.Member;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -22,6 +23,8 @@ public interface CouponService {
     CouponEntity findByCouponCode(Long couponCode);
 
 //    CouponDTO adminRegisterCoupon(AdminCouponRegisterRequestVO request, Admin admin);
+
+    Page<CouponDTO> findCouponsByCampaignCode(FindCampaignDetailDTO campaignDTO, Pageable pageable);
 
     @Transactional
     CouponDTO adminRegisterCoupon(AdminCouponRegisterRequestVO request
