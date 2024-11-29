e3a50542dc4a6cd17b542c2b5f14bfd0988fe036
YuJeeun <dbwpdms1122@naver.com>
2024-11-29 18:21:43 +0900

    ♻️코드 수정: SchedulerConfig -> scheduleCampaigns 수정

--- LearnsMate/src/main/java/intbyte4/learnsmate/common/config/SchedulerConfig.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/common/config/SchedulerConfig.java
@@ -1,8 +1,15 @@
 package intbyte4.learnsmate.common.config;
 
+import intbyte4.learnsmate.admin.service.EmailService;
 import intbyte4.learnsmate.campaign.domain.dto.CampaignDTO;
 import intbyte4.learnsmate.campaign.service.CampaignService;
+import intbyte4.learnsmate.member.domain.MemberType;
+import intbyte4.learnsmate.member.domain.dto.MemberDTO;
+import intbyte4.learnsmate.member.service.MemberService;
+import intbyte4.learnsmate.userpercampaign.domain.dto.UserPerCampaignDTO;
+import intbyte4.learnsmate.userpercampaign.service.UserPerCampaignService;
 import intbyte4.learnsmate.voc.service.VOCAiService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -17,18 +24,25 @@ import java.util.List;
 
 @Configuration
 @EnableScheduling
+@Slf4j
 public class SchedulerConfig {
 
     private final VOCAiService vocAiService;
     private final CampaignService campaignService;
     private final JobLauncher jobLauncher;
     private final Job campaignJob;
+    private final MemberService memberService;
+    private final EmailService emailService;
+    private final UserPerCampaignService userPerCampaignService;
 
-    public SchedulerConfig(VOCAiService vocAiService, CampaignService campaignService, JobLauncher jobLauncher, Job campaignJob) {
+    public SchedulerConfig(VOCAiService vocAiService, CampaignService campaignService, JobLauncher jobLauncher, Job campaignJob, MemberService memberService, EmailService emailService , UserPerCampaignService userPerCampaignService) {
         this.vocAiService = vocAiService;
         this.campaignService = campaignService;
         this.jobLauncher = jobLauncher;
         this.campaignJob = campaignJob;
+        this.memberService = memberService;
+        this.emailService = emailService;
+        this.userPerCampaignService = userPerCampaignService;
     }
 
     @Scheduled(cron = "0 0 9 * * MON")
@@ -46,6 +60,15 @@ public class SchedulerConfig {
                         .addDate("startTime", new Date())
                         .toJobParameters();
                 jobLauncher.run(campaignJob, jobParameters);
+
+                List<UserPerCampaignDTO> userPerCampaignDTOList = userPerCampaignService.findUserByCampaignCode(campaign.getCampaignCode());
+                for (UserPerCampaignDTO userPerCampaignDTO : userPerCampaignDTOList) {
+                    MemberDTO member = memberService.findMemberByMemberCode(userPerCampaignDTO.getStudentCode(), MemberType.STUDENT);
+                    if (member != null) {
+                        emailService.sendCampaignEmail(member.getMemberEmail(), campaign.getCampaignTitle(), campaign.getCampaignContents());
+                    }
+                }
+                campaignService.updateCampaignSendFlag(campaign.getCampaignCode());
             } catch (Exception e) {
                 System.err.println("Failed to launch campaign job: " + e.getMessage());
             }
