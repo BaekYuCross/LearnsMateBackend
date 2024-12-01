1952f53b71df810c8c410a30e0b3f8171fe621f7
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-12-01 15:24:42 +0900

    ✨ 기능 수정 : AppConfig, RedisConfig 어노테이션 수정

--- LearnsMate/src/main/java/intbyte4/learnsmate/common/config/AppConfig.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/common/config/AppConfig.java
@@ -2,12 +2,14 @@ package intbyte4.learnsmate.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableJpaRepositories
 public class AppConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
