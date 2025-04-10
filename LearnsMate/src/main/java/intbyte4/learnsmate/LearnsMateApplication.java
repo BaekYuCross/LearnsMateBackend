c2532404ba1d5310f4f6c344daa7f1bcdc853d98
YuJeeun <dbwpdms1122@naver.com>
2025-04-10 15:21:11 +0900

    ♻️ 기능/코드 수정: prometheus 사용을 위한 설정

--- LearnsMate/src/main/java/intbyte4/learnsmate/LearnsMateApplication.java
+++ LearnsMate/src/main/java/intbyte4/learnsmate/LearnsMateApplication.java
@@ -2,8 +2,10 @@ package intbyte4.learnsmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LearnsMateApplication {
 
     public static void main(String[] args) {
